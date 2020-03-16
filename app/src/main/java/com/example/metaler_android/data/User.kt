714e5747c9c6f8bd4c176fefc8f8d00c4d90cf19
nukeolaf <yhjin04170@gmail.com>
2020-03-16 20:36:17 +0900

    [ADD] User data class 추가
    카카오로 최초 로그인시 카카오 api 로부터 받아온 회원 정보와,
    사용자의 디바이스 모델, 앱 버전 등에 대한 데이터를 정의하는 data class

---
+++ app/src/main/java/com/example/metaler_android/data/User.kt
@@ -0,0 +1,18 @@
+package com.example.metaler_android.data
+
+data class User(
+    val profile_nickname: String,
+    val profile_image_url: String,
+    val profile_email: String,
+    val profile_birthday: String,
+    val profile_gender: String,
+    val job: String,
+    val job_type: String,
+    val job_detail: String,
+    val service_type: Int,
+    val push_token: String,
+    val device_model: String,
+    val device_id: String,
+    val device_os: String,
+    val app_version: String
+)
