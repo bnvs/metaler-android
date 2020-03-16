892196b146c350eba5b65c83c47651b98bb0924a
ohseungyeon <sally37345>
2020-03-16 19:04:50 +0900

    [ADD] 홈 탭에서 상태바 색상 투명하도록 코드 추가함

--- app/src/main/java/com/example/metaler_android/ActivityHome.kt
+++ app/src/main/java/com/example/metaler_android/ActivityHome.kt
@@ -2,11 +2,17 @@ package com.example.metaler_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class ActivityHome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
