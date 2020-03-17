fffa994fad0a93a802dd5b5b9cefdadca9722088
nukeolaf <yhjin04170@gmail.com>
2020-03-18 02:10:22 +0900

    [RF] PostDetail data class 이름 변경
    게시물 상세 내용 관련 데이터 클래스 이름 리팩토링

--- app/src/main/java/com/example/metaler_android/data/PostDetails.kt
+++ app/src/main/java/com/example/metaler_android/data/PostDetails.kt
@@ -1,20 +1,19 @@
 package com.example.metaler_android.data
 
 /**
-* Materials 와 Manufactures 게시물의
- * 상세 내용을 조회하는데 사용하는
- * 데이터 클래스이다
+ * Materials 와 Manufactures 탭에서
+ * 게시물을 클릭하여
+ * 게시물 상세 내용을 조회할 때 사용하는
+ * 데이터 클래스
 * */
 
-// TODO : PostDetail 과 Post 명명법이 적절한지 논의 필요
-
-// TODO : 스프레드 시트에 작성된 내용에 의하면, Post JSON 객체를 배열로 감싸서 반환하던데 그 이유가 무엇인지 질문
-data class PostDetail(
-    val Post: List<Post>
+// TODO : 스프레드 시트에 작성된 내용에 의하면, PostDetail JSON 객체를 배열로 감싸서 반환하던데 그 이유가 무엇인지 질문
+data class PostDetails(
+    val Post: List<PostDetail>
 )
 
 // TODO : tags 가 두 개 들어있는데 둘 의 차이가 무엇인지?
-data class Post(
+data class PostDetail(
     val post_id: Int,
     val title: String,
     val nickname: Int,
