10ac986e874a26e6be9ee8d523732bb5027caeb3
nukeolaf <yhjin04170@gmail.com>
2020-03-18 02:30:10 +0900

    [RF] Posts 와 Post 데이터 클래스 분리

---
+++ app/src/main/java/com/example/metaler_android/data/Post.kt
@@ -0,0 +1,18 @@
+package com.example.metaler_android.data
+
+/**
+ * Materials, Manufactures, Bookmarks, 내가쓴 글 확인 탭에서
+ * 게시물의 리스트를 구성하는 각각의 게시물 데이터를 정의하는 데이터 클래스
+* */
+
+data class Post(
+    val post_id: Int,
+    val title: String,
+    val nickname: Int,
+    val date: String,
+    val tags: List<String>,
+    val image_url: String,
+    val like: Int,
+    val dis_like: Int,
+    val is_bookmark: Boolean
+)
--- app/src/main/java/com/example/metaler_android/data/Posts.kt
+++ app/src/main/java/com/example/metaler_android/data/Posts.kt
@@ -11,17 +11,5 @@ package com.example.metaler_android.data
 data class Posts(
     val is_next: Boolean,
     val post_count: Int,
-    val posts: List<PostsItem>
-)
-
-data class PostsItem(
-    val post_id: Int,
-    val title: String,
-    val nickname: Int,
-    val date: String,
-    val tags: List<String>,
-    val image_url: String,
-    val like: Int,
-    val dis_like: Int,
-    val is_bookmark: Boolean
+    val posts: List<Post>
 )
