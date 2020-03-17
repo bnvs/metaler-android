e20d01081cc701392523a8f10dd24b820da583ef
nukeolaf <yhjin04170@gmail.com>
2020-03-17 22:32:35 +0900

    [ADD] Posts data class 설명 주석 추가

--- app/src/main/java/com/example/metaler_android/data/Posts.kt
+++ app/src/main/java/com/example/metaler_android/data/Posts.kt
@@ -1,11 +1,20 @@
 package com.example.metaler_android.data
 
+/**
+* Materials 와 Manufactures 탭에서
+ * 게시물의 리스트를 보여주는데 사용하는
+ * 데이터 클래스이다
+ * is_next 는 다음 페이지의 유무
+ * post_count 는 게시글 총 개수
+* */
+
 data class Posts(
     val is_next: Boolean,
     val post_count: Int,
     val posts: List<PostsItem>
 )
 
+// TODO : PostsItem 이라는 명명법이 적절한지 논의 필요
 data class PostsItem(
     val post_id: Int,
     val title: String,
