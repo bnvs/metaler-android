490ffd2570ad51d4f7b1a141f35a64c9bf36fc5e
ohseungyeon <sally37345>
2020-03-17 14:42:04 +0900

    [ADD] 가공 카드뷰 레이아웃 추가함

--- app/src/main/java/com/example/metaler_android/ActivityHome.kt
+++ app/src/main/java/com/example/metaler_android/ActivityHome.kt
@@ -2,14 +2,19 @@ package com.example.metaler_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_home.*
 
 class ActivityHome : AppCompatActivity() {
+
+    val TAG = "ActivityHome"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        //상태바 투명하게 바꾸는 코드
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
