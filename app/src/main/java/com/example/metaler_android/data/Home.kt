4241f6c659bbfa27e0866f464f440291aeeaa170
nukeolaf <yhjin04170@gmail.com>
2020-03-17 21:59:40 +0900

    [ADD] Home data class 설명 주석 추가

--- app/src/main/java/com/example/metaler_android/data/Home.kt
+++ app/src/main/java/com/example/metaler_android/data/Home.kt
@@ -1,10 +1,17 @@
 package com.example.metaler_android.data
 
+/**
+* Home 액티비티에서
+ * 재료와 가공의 최신 게시물을 보여주는 데 사용하는
+ * 데이터 클래스
+* */
+
 data class Home(
     val materials: List<HomePost>,
     val manufactures: List<HomePost>
 )
 
+// TODO : HomePost 라는 명명법이 적절한지 논의 필요
 data class HomePost(
     val post_id: Int,
     val title: String,
