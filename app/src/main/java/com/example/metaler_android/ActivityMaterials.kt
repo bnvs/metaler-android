4b63fa4045d21ec8686de5da2e3b1121c1c6a68d
ohseungyeon <sally37345>
2020-03-19 03:39:50 +0900

    [ADD]카테고리 버튼 클릭 시 디자인 요소 변경되는지 테스트하기 위해 테스트용으로 코드 추가함

--- app/src/main/java/com/example/metaler_android/ActivityMaterials.kt
+++ app/src/main/java/com/example/metaler_android/ActivityMaterials.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_home.homeBtn
 import kotlinx.android.synthetic.main.activity_materials.*
@@ -22,7 +24,7 @@ class ActivityMaterials : AppCompatActivity() {
 
         //카테고리 버튼 색상 초기화
         // 활성화된 버튼만 글자 색상 변경하고, 비활성화된 버튼은 백그라운드소스 없애기
-        allBtn.setTextColor(resources.getColor(R.color.colorPurple))
+        allBtn.setTextColor(ContextCompat.getColor(this,R.color.colorLightGrey))
         cooperBtn.setBackgroundResource(0)
         stainlessBtn.setBackgroundResource(0)
         aluminiumBtn.setBackgroundResource(0)
@@ -32,5 +34,41 @@ class ActivityMaterials : AppCompatActivity() {
         chemicalBtn.setBackgroundResource(0)
         othersBtn.setBackgroundResource(0)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+
+        cooperBtn.setOnClickListener {
+            allBtn.setBackgroundResource(0)
+            allBtn.setTextColor(ContextCompat.getColor(this,R.color.colorLightGrey))
+            cooperBtn.setTextColor(ContextCompat.getColor(this,R.color.colorPurple))
+            cooperBtn.background = ContextCompat.getDrawable(this,R.drawable.active_bar)
+            stainlessBtn.setBackgroundResource(0)
+            aluminiumBtn.setBackgroundResource(0)
+            nickelBtn.setBackgroundResource(0)
+            steelBtn.setBackgroundResource(0)
+            toolsBtn.setBackgroundResource(0)
+            chemicalBtn.setBackgroundResource(0)
+            othersBtn.setBackgroundResource(0)
+        }
+
+        stainlessBtn.setOnClickListener {
+            allBtn.setBackgroundResource(0)
+            allBtn.setTextColor(ContextCompat.getColor(this,R.color.colorLightGrey))
+            cooperBtn.setBackgroundResource(0)
+            cooperBtn.setTextColor(ContextCompat.getColor(this,R.color.colorLightGrey))
+            stainlessBtn.setTextColor(ContextCompat.getColor(this,R.color.colorPurple))
+            stainlessBtn.background = ContextCompat.getDrawable(this,R.drawable.active_bar)
+            aluminiumBtn.setBackgroundResource(0)
+            nickelBtn.setBackgroundResource(0)
+            steelBtn.setBackgroundResource(0)
+            toolsBtn.setBackgroundResource(0)
+            chemicalBtn.setBackgroundResource(0)
+            othersBtn.setBackgroundResource(0)
+
+        }
     }
 }
