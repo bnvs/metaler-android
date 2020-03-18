ddc1ff292711298b9d4cc71b64ebf8202baad1b5
nukeolaf <yhjin04170@gmail.com>
2020-03-19 02:21:46 +0900

    [ADD] PostDetailDataSource 인터페이스 추가
    게시물 상세 내용 관련된 데이터 관리

--- app/src/main/java/com/example/metaler_android/data/PostDetail/source/PostDetailDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/PostDetail/source/PostDetailDataSource.kt
@@ -1,2 +1,14 @@
 package com.example.metaler_android.data.PostDetail.source
 
+import com.example.metaler_android.data.PostDetail.PostDetails
+
+interface PostDetailDataSource {
+
+    interface LoadPostDetailsCallback {
+        fun onPostDetailsLoaded(postDetails: PostDetails)
+        fun onDataNotAvailable()
+    }
+
+    fun getPostDetails(callback: LoadPostDetailsCallback)
+
+}
