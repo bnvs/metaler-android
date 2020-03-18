c2edf9036851b7a95ab79952554998bd43ae4cb4
nukeolaf <yhjin04170@gmail.com>
2020-03-18 21:16:57 +0900

    [ADD] DataSource - LoadMaterialsCallback 인터페이스 추가
    재료 탭의 카테고리 리스트를 가져왔을 때 실행되는 콜백 인터페이스

--- app/src/main/java/com/example/metaler_android/data/source/DataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/source/DataSource.kt
@@ -1,11 +1,17 @@
 package com.example.metaler_android.data.source
 
 import com.example.metaler_android.data.HomePosts
+import com.example.metaler_android.data.Materials
 import com.example.metaler_android.data.Posts
 
 interface DataSource {
     interface LoadHomePostsCallback {
         fun onHomePostsLoaded(homePosts: HomePosts)
+        fun onDataNotAvailable()
+    }
+
+    interface LoadMaterialsCallback {
+        fun onMaterialsLoaded(materials: Materials)
         fun onDataNotAvailable()
     }
 
