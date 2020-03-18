16c74b431e3b1f25f816a8f024761393c2351b91
nukeolaf <yhjin04170@gmail.com>
2020-03-19 03:27:53 +0900

    [FIX] PostLocalDataSource 이 아니라 PostRemoteDataSource 에 작성해야하는 내용이었음

--- app/src/main/java/com/example/metaler_android/data/post/source/local/PostLocalDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/post/source/local/PostLocalDataSource.kt
@@ -1,28 +1,5 @@
 package com.example.metaler_android.data.post.source.local
 
-import com.example.metaler_android.data.post.Posts
-import com.example.metaler_android.data.post.source.PostDataSource
-import com.example.metaler_android.network.RetrofitClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
-class PostLocalDataSource : PostDataSource{
-
-    private val retrofitClient = RetrofitClient.client
-
-    override fun getPosts(callback: PostDataSource.LoadPostsCallback) {
-        retrofitClient.getMaterialPosts().enqueue(object : Callback<Posts> {
-            override fun onResponse(call: Call<Posts>, response: Response<Posts>) {
-                val posts: Posts? = response.body()
-                callback.onPostsLoaded(posts!!)
-            }
-
-            override fun onFailure(call: Call<Posts>, t: Throwable) {
-                callback.onDataNotAvailable()
-            }
-
-        })
-    }
-
+class PostLocalDataSource {
+    //
 }
--- app/src/main/java/com/example/metaler_android/data/post/source/remote/PostRemoteDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/post/source/remote/PostRemoteDataSource.kt
@@ -1,5 +1,26 @@
 package com.example.metaler_android.data.post.source.remote
 
-class PostRemoteDataSource {
-    //
+import com.example.metaler_android.data.post.Posts
+import com.example.metaler_android.data.post.source.PostDataSource
+import com.example.metaler_android.network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class PostRemoteDataSource : PostDataSource{
+    private val retrofitClient = RetrofitClient.client
+
+    override fun getPosts(callback: PostDataSource.LoadPostsCallback) {
+        retrofitClient.getMaterialPosts().enqueue(object : Callback<Posts> {
+            override fun onResponse(call: Call<Posts>, response: Response<Posts>) {
+                val posts: Posts? = response.body()
+                callback.onPostsLoaded(posts!!)
+            }
+
+            override fun onFailure(call: Call<Posts>, t: Throwable) {
+                callback.onDataNotAvailable()
+            }
+
+        })
+    }
 }
