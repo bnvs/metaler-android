51ce86fba6f58fb2ddaf81352d5c7a0f0a956aa4
nukeolaf <yhjin04170@gmail.com>
2020-03-19 02:32:43 +0900

    [ADD] JobDataSource 인터페이스 추가
    사용자 소속 데이터 가져오는 data source

--- app/src/main/java/com/example/metaler_android/data/comment/source/CommentDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/comment/source/CommentDataSource.kt
@@ -3,12 +3,12 @@ package com.example.metaler_android.data.comment.source
 import com.example.metaler_android.data.comment.Comments
 
 interface CommentDataSource {
-
+   
     interface LoadCommentsCallback {
         fun onCommentsLoaded(comments: Comments)
         fun onDataNotAvailable()
     }
     
     fun getComments(callback: LoadCommentsCallback)
-
+    
 }
--- app/src/main/java/com/example/metaler_android/data/job/source/JobDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/job/source/JobDataSource.kt
@@ -1,2 +1,15 @@
 package com.example.metaler_android.data.job.source
 
+import com.example.metaler_android.data.job.Job
+import org.json.JSONObject
+
+interface JobDataSource {
+
+    interface LoadJobCallback {
+        fun onJobLoaded(job: Job)
+        fun onDataNotAvailable()
+    }
+
+    fun getJob(callback: LoadJobCallback)
+    
+}
