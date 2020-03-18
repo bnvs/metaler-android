9e30e82a3b51ef2bc6bbf78d1772e7094e10affc
nukeolaf <yhjin04170@gmail.com>
2020-03-18 21:05:13 +0900

    [ADD] DataSource - LoadHomePostsCallback 인터페이스 추가
    HomePosts 데이터를 가져왔을때 실행되는 콜백 인터페이스

--- app/src/main/java/com/example/metaler_android/data/source/DataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/source/DataSource.kt
@@ -1,5 +1,11 @@
 package com.example.metaler_android.data.source
 
+import com.example.metaler_android.data.HomePosts
+import com.example.metaler_android.data.Posts
+
 interface DataSource {
-    //
+    interface LoadHomePostsCallback {
+        fun onHomePostsLoaded(homePosts: HomePosts)
+        fun onDataNotAvailable()
+    }
 }
