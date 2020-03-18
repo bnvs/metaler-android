c361b8de73c4a0e850b08ab9d97df5202feb3a2f
nukeolaf <yhjin04170@gmail.com>
2020-03-19 02:19:49 +0900

    [ADD] UserDataSource 인터페이스 추가
    유저 데이터 추가/삭제

--- app/src/main/java/com/example/metaler_android/data/User/source/UserDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/User/source/UserDataSource.kt
@@ -1,2 +1,14 @@
 package com.example.metaler_android.data.User.source
 
+import org.json.JSONObject
+
+interface UserDataSource {
+
+    interface AddUserCallback {
+        fun onUserAdded(user: JSONObject)
+        fun onFailure()
+    }
+
+    fun addUser(callback: AddUserCallback)
+
+}
