9a1ae6c0fb80aa696d7f4db8c52d58e31bf174f0
ohseungyeon <sally37345>
2020-03-19 03:06:56 +0900

    [ADD]재료탭 활성화된 카테고리 버튼만 색상 변경되도록 코드 추가함

--- app/src/main/java/com/example/metaler_android/ActivityMaterials.kt
+++ app/src/main/java/com/example/metaler_android/ActivityMaterials.kt
@@ -1,9 +1,12 @@
 package com.example.metaler_android
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.homeBtn
+import kotlinx.android.synthetic.main.activity_materials.*
 
 class ActivityMaterials : AppCompatActivity() {
 
@@ -16,5 +19,18 @@ class ActivityMaterials : AppCompatActivity() {
             val goToHome = Intent(this, ActivityHome::class.java)
             startActivity(goToHome)
         }
+
+        //카테고리 버튼 색상 초기화
+        // 활성화된 버튼만 글자 색상 변경하고, 비활성화된 버튼은 백그라운드소스 없애기
+        allBtn.setTextColor(resources.getColor(R.color.colorPurple))
+        cooperBtn.setBackgroundResource(0)
+        stainlessBtn.setBackgroundResource(0)
+        aluminiumBtn.setBackgroundResource(0)
+        nickelBtn.setBackgroundResource(0)
+        steelBtn.setBackgroundResource(0)
+        toolsBtn.setBackgroundResource(0)
+        chemicalBtn.setBackgroundResource(0)
+        othersBtn.setBackgroundResource(0)
+
     }
 }
