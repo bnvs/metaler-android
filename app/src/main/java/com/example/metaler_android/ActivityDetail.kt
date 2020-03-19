77fdbda71afc6135ced1800fd26ad8c4d8c6f6f3
ohseungyeon <sally37345>
2020-03-20 02:25:21 +0900

    [ADD] 재료탭, 가공탭의 상세페이지 레이아웃이 같다는 주석 추가함

--- app/src/main/java/com/example/metaler_android/ActivityDetail.kt
+++ app/src/main/java/com/example/metaler_android/ActivityDetail.kt
@@ -3,6 +3,8 @@ package com.example.metaler_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//재료탭, 가공탭의 상세페이지 레이아웃이 같기 떄문에 같이 사용한다.
 class ActivityDetail : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
