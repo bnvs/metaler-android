c2435b5a06526489b6d9315d9eec78fcd78c7f0a
nukeolaf <yhjin04170@gmail.com>
2020-03-20 10:03:38 +0900

    [RF] image_url -> attach_urls 변수명 변경
    attach_urls 와 color 는 기획상 빠졌지만
    다시 추가될 수 있기에 일단 냅두는것, 빈값으로 넘기면 됨

--- app/src/main/java/com/example/metaler_android/data/category/Category.kt
+++ app/src/main/java/com/example/metaler_android/data/category/Category.kt
@@ -3,6 +3,6 @@ package com.example.metaler_android.data.category
 data class Category(
     val category_id: Int,
     val name: String,
-    val image_url: String,
+    val attach_urls: String,
     val color: String
 )
