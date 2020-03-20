f6f8f541b7160f68daed1ccd44b94a99f2ff865f
nukeolaf <yhjin04170@gmail.com>
2020-03-20 10:16:45 +0900

    [ADD] 임시로 jsonObject 만들어서 레트로핏 함수 파라미터에 인자로 넘김

--- app/src/main/java/com/example/metaler_android/data/post/source/remote/PostRemoteDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/post/source/remote/PostRemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.example.metaler_android.data.post.source.remote
 import com.example.metaler_android.data.post.Posts
 import com.example.metaler_android.data.post.source.PostDataSource
 import com.example.metaler_android.network.RetrofitClient
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -10,8 +11,11 @@ import retrofit2.Response
 class PostRemoteDataSource : PostDataSource{
     private val retrofitClient = RetrofitClient.client
 
+    // 임시로 작성한 json object
+    var json: JSONObject = JSONObject()
+
     override fun getPosts(callback: PostDataSource.LoadPostsCallback) {
-        retrofitClient.getPosts().enqueue(object : Callback<Posts> {
+        retrofitClient.getPosts("material", json).enqueue(object : Callback<Posts> {
             override fun onResponse(call: Call<Posts>, response: Response<Posts>) {
                 val posts: Posts? = response.body()
                 if (posts != null) {
