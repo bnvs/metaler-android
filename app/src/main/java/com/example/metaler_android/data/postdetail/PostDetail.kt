b0753be2e05f8810e2382d7d80c9e6f0fd5edd88
nukeolaf <yhjin04170@gmail.com>
2020-03-20 10:34:59 +0900

    [MD] PostDetail 데이터 클래스 변수 수정
    attachs -> attach_urls
    comment_count -> comments_count
    price_type 추가
    tags 배열 중복 제거

--- app/src/main/java/com/example/metaler_android/data/postdetail/PostDetail.kt
+++ app/src/main/java/com/example/metaler_android/data/postdetail/PostDetail.kt
@@ -1,20 +1,18 @@
 package com.example.metaler_android.data.postdetail
 
-// TODO : tags 가 두 개 들어있는데 둘 의 차이가 무엇인지?
 data class PostDetail(
     val post_id: Int,
     val title: String,
     val nickname: Int,
     val date: String,
-    val tags: List<String>,
-    val image_url: String,
     val like: Int,
     val dis_like: Int,
     val is_bookmark: Boolean,
     val content: String,
-    val attachs: List<String>,
-    // val tags: List<String>,
-    val comment_count: Int,
+    val attach_urls: List<String>,
+    val tags: List<String>,
+    val comments_count: Int,
     val price: Int,
+    val price_type: String,
     val user_rating: String
 )
