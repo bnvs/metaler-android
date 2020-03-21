9aed55d9c6f3ce41e3999a825f43c5f68a788433
nukeolaf <yhjin04170@gmail.com>
2020-03-22 04:48:13 +0900

    [ADD] ActivityManufactures 에 ContractManufactures.View 구현

--- app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
+++ app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
@@ -1,13 +1,46 @@
 package com.example.metaler_android.manufactures
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.metaler_android.R
 
-class ActivityManufactures : AppCompatActivity() {
+class ActivityManufactures : AppCompatActivity(), ContractManufactures.View {
+
+    override var presenter: ContractManufactures.Presenter
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manufacture)
+    }
+
+    override fun showPosts() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showPostDetailUi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showSearchUi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showSearchTags() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun clearSearchTagBar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteSearchTag() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setTapBarListener(context: Context) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
