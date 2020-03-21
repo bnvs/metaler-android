56e132bf9ab3bada9776d6fd0c9366ef7fbf81b6
nukeolaf <yhjin04170@gmail.com>
2020-03-22 08:50:04 +0900

    [ADD] JobLocalDataSource 와 JobRemoteDataSource 추가

--- app/src/main/java/com/example/metaler_android/data/job/source/JobRepository.kt
+++ app/src/main/java/com/example/metaler_android/data/job/source/JobRepository.kt
@@ -1,5 +1,7 @@
 package com.example.metaler_android.data.job.source
 
-object JobRepository {
-    //
+class JobRepository : JobDataSource{
+    override fun getJob(callback: JobDataSource.LoadJobCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
