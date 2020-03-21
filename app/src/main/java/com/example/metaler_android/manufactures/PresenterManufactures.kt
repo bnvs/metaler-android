1e1dab1e312e8f977c5863c037eeda570c152f44
nukeolaf <yhjin04170@gmail.com>
2020-03-22 04:49:29 +0900

    [ADD] PresenterManufactures 에 ContractManufactures.Presenter 구현

--- app/src/main/java/com/example/metaler_android/manufactures/PresenterManufactures.kt
+++ app/src/main/java/com/example/metaler_android/manufactures/PresenterManufactures.kt
@@ -1,5 +1,37 @@
 package com.example.metaler_android.manufactures
 
-class PresenterManufactures {
-    //
+import android.content.Context
+
+class PresenterManufactures : ContractManufactures.Presenter {
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadPosts() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openPostDetail() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openSearch() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun AddsearchTag() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun clearSearchTagBar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteSearchTag() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setTapBar(context: Context) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
