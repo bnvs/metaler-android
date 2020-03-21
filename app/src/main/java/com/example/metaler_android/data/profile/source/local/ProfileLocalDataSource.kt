7abbd837f6ba2e903aa39ef292d2fdca358b8747
nukeolaf <yhjin04170@gmail.com>
2020-03-21 22:30:50 +0900

    [ADD] ProfileLocalDataSource 가 SharedPreference 를 사용하도록 코드 추가

--- app/src/main/java/com/example/metaler_android/data/profile/source/local/ProfileLocalDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/profile/source/local/ProfileLocalDataSource.kt
@@ -1,11 +1,15 @@
 package com.example.metaler_android.data.profile.source.local
 
+import android.content.Context
 import com.example.metaler_android.data.profile.source.ProfileDataSource
 
-class ProfileLocalDataSource : ProfileDataSource {
+class ProfileLocalDataSource (context: Context) : ProfileDataSource {
+
+    private val sharedPreferences = context.getSharedPreferences("profile", Context.MODE_PRIVATE)
+    private val editor = sharedPreferences.edit()
 
     override fun getProfile(callback: ProfileDataSource.LoadProfileCallback) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-    
+
 }
--- app/src/main/java/com/example/metaler_android/data/profile/source/remote/ProfileRemoteDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/profile/source/remote/ProfileRemoteDataSource.kt
@@ -4,6 +4,7 @@ import com.example.metaler_android.data.profile.source.ProfileDataSource
 import com.example.metaler_android.network.RetrofitClient
 
 object ProfileRemoteDataSource : ProfileDataSource {
+
     private val retrofitClient = RetrofitClient.client
 
     override fun getProfile(callback: ProfileDataSource.LoadProfileCallback) {
