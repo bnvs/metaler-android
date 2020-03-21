7bad6323a6ff6c318faeac6ff44213aa310c6324
nukeolaf <yhjin04170@gmail.com>
2020-03-22 05:02:09 +0900

    [ADD] ActivityHome setTapBarListener() 함수 작성

--- app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
+++ app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
@@ -1,9 +1,13 @@
 package com.example.metaler_android.manufactures
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.metaler_android.R
+import com.example.metaler_android.home.ActivityHome
+import com.example.metaler_android.materials.ActivityMaterials
+import kotlinx.android.synthetic.main.activity_manufacture.*
 
 class ActivityManufactures : AppCompatActivity(), ContractManufactures.View {
 
@@ -42,7 +46,38 @@ class ActivityManufactures : AppCompatActivity(), ContractManufactures.View {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    // 하단 탭 바에 리스너를 추가한다
     override fun setTapBarListener(context: Context) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        homeIcon.setOnClickListener {
+            Intent(context, ActivityHome::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        materialsIcon.setOnClickListener {
+            Intent(context, ActivityMaterials::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        manufactureIcon.setOnClickListener {
+            Intent(context, ActivityManufactures::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        /*bookmarkIcon.setOnClickListener {
+            Intent(context, ActivityBookmarks::class.java).also {
+                addFlags(it)
+                startActivity(it)
+            }
+        }
+
+        myPageIcon.setOnClickListener {
+            Intent(context, ActivityMyPage::class.java).also {
+                addFlags(it)
+                startActivity(it)
+            }
+        }*/
     }
 }
