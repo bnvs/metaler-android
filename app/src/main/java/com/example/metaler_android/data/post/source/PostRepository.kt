834e2d3d92d4fbe5516f08f6f8c13b45de56fff6
nukeolaf <yhjin04170@gmail.com>
2020-03-22 05:12:02 +0900

    [ADD] PostRepository 에 local 과 remote DataSource 추가

--- app/src/main/java/com/example/metaler_android/data/post/source/PostRepository.kt
+++ app/src/main/java/com/example/metaler_android/data/post/source/PostRepository.kt
@@ -1,6 +1,14 @@
 package com.example.metaler_android.data.post.source
 
-class PostRepository : PostDataSource {
+import android.content.Context
+import com.example.metaler_android.data.post.source.local.PostLocalDataSource
+import com.example.metaler_android.data.post.source.remote.PostRemoteDataSource
+
+class PostRepository(context: Context) : PostDataSource {
+
+    val postLocalDataSource = PostLocalDataSource(context)
+    val postRemoteDataSource = PostRemoteDataSource
+
     override fun getPosts(callback: PostDataSource.LoadPostsCallback) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
