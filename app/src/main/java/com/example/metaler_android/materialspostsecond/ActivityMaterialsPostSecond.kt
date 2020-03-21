4f49a81cdd9177a9bfa44cae7ca2b3346735f2c7
ohseungyeon <sally37345>
2020-03-22 02:57:58 +0900

    [ADD] ActivityMaterialsPostSecond(재료 글작성2단계) 액티비티 추가함

---
+++ app/src/main/java/com/example/metaler_android/materialspostsecond/ActivityMaterialsPostSecond.kt
@@ -0,0 +1,13 @@
+package com.example.metaler_android.materialspostsecond
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.metaler_android.R
+
+class ActivityMaterialsPostSecond : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materials_post_second)
+    }
+}
