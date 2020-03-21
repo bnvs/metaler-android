a22a588a6d4ef8084a80571336838f9b93101ed3
nukeolaf <yhjin04170@gmail.com>
2020-03-22 07:29:46 +0900

    [ADD] CommentRepository 에 CommentDataSource implement

--- app/src/main/java/com/example/metaler_android/data/comment/source/CommentRepository.kt
+++ app/src/main/java/com/example/metaler_android/data/comment/source/CommentRepository.kt
@@ -1,5 +1,7 @@
 package com.example.metaler_android.data.comment.source
 
-object CommentRepository {
-    //
+class CommentRepository : CommentDataSource{
+    override fun getComments(callback: CommentDataSource.LoadCommentsCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
