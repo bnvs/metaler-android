6cb0925200010ac3f36b046d773d7d328e2a0231
nukeolaf <yhjin04170@gmail.com>
2020-03-22 04:17:28 +0900

    [ADD] ContractMaterials 인터페이스에 에 View 와 Presenter 기능 작성

--- app/src/main/java/com/example/metaler_android/materials/ContractMaterials.kt
+++ app/src/main/java/com/example/metaler_android/materials/ContractMaterials.kt
@@ -1,11 +1,40 @@
 package com.example.metaler_android.materials
 
+import android.content.Context
+import com.example.metaler_android.BasePresenter
+import com.example.metaler_android.BaseView
+
+/**
+ * Contract interface 는
+ * View 와 Presenter 에 사용될 함수를 정의하고 관리하는데 사용
+ * */
+
 interface ContractMaterials {
-    interface View {
-        //
+    interface View : BaseView<Presenter>{
+        fun showCategories()
+
+        fun showPosts()
+
+        fun showPostDetailUi()
+
+        fun showSearchUi()
+
+        fun showSearchTags()
+
+        fun setTapBarListener(context: Context)
     }
 
-    interface Presenter {
-        //
+    interface Presenter : BasePresenter {
+        fun loadCategories()
+
+        fun loadPosts()
+
+        fun openPostDetail()
+
+        fun openSearch()
+
+        fun searchTag()
+
+        fun setTapBar(context: Context)
     }
 }
