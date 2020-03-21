b5409e576e5041e754bd3b35f09ac43969fd5311
nukeolaf <yhjin04170@gmail.com>
2020-03-22 08:15:10 +0900

    [RF] PostAdapter 리사이클러뷰 어댑터 모듈화
    해당 adapter 가 manufactures, materials 탭 두 곳에서 재사용 가능하여 모듈화함

--- app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
+++ app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
@@ -4,16 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.metaler_android.R
 import com.example.metaler_android.data.post.Post
 import com.example.metaler_android.home.ActivityHome
 import com.example.metaler_android.materials.ActivityMaterials
+import com.example.metaler_android.util.PostAdapter
 import kotlinx.android.synthetic.main.activity_manufacture.*
 import kotlinx.android.synthetic.main.item_posts_rv.view.*
 
@@ -140,71 +137,7 @@ class ActivityManufactures : AppCompatActivity(), ContractManufactures.View {
         }*/
     }
 
-    /**
-     * 가공 탭의 리사이클러뷰에 사용할 어댑터입니다.
-     * 재료 탭의 리사이클러뷰에 재사용 가능하게 작성할 예정입니다
-     * */
-    private class PostAdapter(
-        private var posts: List<Post>,
-        private val itemListener: PostItemListener
-    ) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
-
-        fun setPosts(list: List<Post>) {
-            this.posts = list
-        }
-
-        fun setBookmark(position: Int) {
-            posts[position].is_bookmark = !posts[position].is_bookmark
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val inflatedView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_posts_rv, parent, false)
-            return ViewHolder(inflatedView)
-        }
-
-        override fun getItemCount(): Int {
-            return posts.size
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.bind(posts[position], position)
-        }
-
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private var view: View = itemView
-
-            fun bind(item: Post, position: Int) {
-
-                var tags = ""
-                for (tag in item.tags) {
-                    tags += "#$tag "
-                }
-
-                view.apply {
-                    title.text = item.title
-                    userName.text = item.nickname
-                    date.text = item.date
-                    // tagRV.text = tags TODO : 태그 보여주는 방식 논의 필요
-                    dislikeNum.text = item.dis_like.toString()
-                    likeNum.text = item.like.toString()
-                    setOnClickListener { itemListener.onPostClick(item.post_id) }
-                    bookmarkBtn.setOnClickListener {
-                        itemListener.onBookmarkButtonClick(view, item.post_id, item.is_bookmark, position)
-                    }
-                    if (item.is_bookmark) {
-                        bookmarkBtn.setImageResource(R.drawable.ic_list_bookmark_active_x3)
-                    }
-                }
-
-                Glide.with(view)
-                    .load(item.attach_url)
-                    .into(view.img)
-            }
-        }
-    }
-
-    private interface PostItemListener {
+    interface PostItemListener {
         fun onPostClick(clickedPostId: Int)
 
         fun onBookmarkButtonClick( view: View, clickedPostId: Int, isBookmark: Boolean, position: Int)
