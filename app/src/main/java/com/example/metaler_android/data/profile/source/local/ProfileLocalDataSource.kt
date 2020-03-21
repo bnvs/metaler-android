f0fbe9f88d1c1131eb2483ab81d957d3ac621d93
nukeolaf <yhjin04170@gmail.com>
2020-03-21 21:35:59 +0900

    [ADD] ProfileLocalDataSource 클래스 추가

---
+++ app/src/main/java/com/example/metaler_android/data/profile/source/local/ProfileLocalDataSource.kt
@@ -0,0 +1,9 @@
+package com.example.metaler_android.data.profile.source.local
+
+import com.example.metaler_android.data.profile.source.ProfileDataSource
+
+class ProfileLocalDataSource : ProfileDataSource {
+    override fun getProfile(callback: ProfileDataSource.LoadProfileCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
