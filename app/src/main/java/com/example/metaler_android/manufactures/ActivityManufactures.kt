fe926cb5cb82812883eb234e160aa9cf27bb5f50
nukeolaf <yhjin04170@gmail.com>
2020-03-21 19:54:28 +0900

    [ADD] 각 액티비티마다 Contract 와 Presenter 파일 만들어둠

--- app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
+++ app/src/main/java/com/example/metaler_android/manufactures/ActivityManufactures.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.metaler_android.R
 
-class ActivityManufacture : AppCompatActivity() {
+class ActivityManufactures : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
