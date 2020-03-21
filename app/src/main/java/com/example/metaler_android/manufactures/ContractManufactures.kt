a8bdfe96101200f8ac16e546ca5436d9ef9bc30c
nukeolaf <yhjin04170@gmail.com>
2020-03-22 04:37:56 +0900

    [ADD] ContractManufactures 의 View 와 Presenter 에 BaseView 와 BasePresenter 구현

--- app/src/main/java/com/example/metaler_android/manufactures/ContractManufactures.kt
+++ app/src/main/java/com/example/metaler_android/manufactures/ContractManufactures.kt
@@ -1,11 +1,19 @@
 package com.example.metaler_android.manufactures
 
+import com.example.metaler_android.BasePresenter
+import com.example.metaler_android.BaseView
+
+/**
+ * Contract interface 는
+ * View 와 Presenter 에 사용될 함수를 정의하고 관리하는데 사용
+ * */
+
 interface ContractManufactures {
-    interface View {
+    interface View : BaseView<Presenter> {
         //
     }
 
-    interface Presenter {
+    interface Presenter : BasePresenter {
         //
     }
 }
