2bf010e13594438f16a04150ecc3dd61c65d7fa4
nukeolaf <yhjin04170@gmail.com>
2020-03-22 07:47:03 +0900

    [RF] PostDetailRepository 에 LocalDataSource 와 RemoteDataSource 추가

--- app/src/main/java/com/example/metaler_android/data/postdetail/source/PostDetailRepository.kt
+++ app/src/main/java/com/example/metaler_android/data/postdetail/source/PostDetailRepository.kt
@@ -1,6 +1,14 @@
 package com.example.metaler_android.data.postdetail.source
 
-class PostDetailRepository : PostDetailDataSource {
+import android.content.Context
+import com.example.metaler_android.data.postdetail.source.local.PostDetailsLocalDataSource
+import com.example.metaler_android.data.postdetail.source.remote.PostDetailsRemoteDataSource
+
+class PostDetailRepository(context: Context) : PostDetailDataSource {
+
+    private val postDetailsLocalDataSource = PostDetailsLocalDataSource(context)
+    private val postDetialsRemoteDataSource = PostDetailsRemoteDataSource
+
     override fun getPostDetails(callback: PostDetailDataSource.LoadPostDetailsCallback) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
