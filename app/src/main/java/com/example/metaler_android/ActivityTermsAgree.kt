aa9581a93831a37584f266ceeecdc2ab26044ae1
ohseungyeon <sally37345>
2020-03-23 01:32:02 +0900

    [ADD] ActivityTermsAgree(이용약관 동의받는 액티비티) 추가함

---
+++ app/src/main/java/com/example/metaler_android/ActivityTermsAgree.kt
@@ -0,0 +1,12 @@
+package com.example.metaler_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTermsAgree : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_agree)
+    }
+}
