270cce09b5a3a9d60ba93c435cf1280930ec62f7
ohseungyeon <sally37345>
2020-03-22 17:42:26 +0900

    [ADD]ActivityBookmark 추가함

---
+++ app/src/main/java/com/example/metaler_android/bookmark/ActivityBookmark.kt
@@ -0,0 +1,13 @@
+package com.example.metaler_android.bookmark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.metaler_android.R
+
+class ActivityBookmark : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+    }
+}
