0fa6603d1d6efb4b29cd78a38c5cecebbdc7dc95
ohseungyeon <sally37345>
2020-03-22 17:36:45 +0900

    [ADD] ActivityManufacturesPostSecond (가공 글작성 2단계 액티비티) 추가함

---
+++ app/src/main/java/com/example/metaler_android/manufacturespostsecond/ActivityManufacturesPostSecond.kt
@@ -0,0 +1,13 @@
+package com.example.metaler_android.manufacturespostsecond
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.metaler_android.R
+
+class ActivityManufacturesPostSecond : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manufactures_post_second)
+    }
+}
