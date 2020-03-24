65203e418b145f52ce1c7543c99cbb84712ed9f7
ohseungyeon <sally37345>
2020-03-24 20:39:55 +0900

    [FIX] ContractMyPage.kt 인터페이스 이름 틀려서 고침

--- app/src/main/java/com/example/metaler_android/mypage/ContractMyPage.kt
+++ app/src/main/java/com/example/metaler_android/mypage/ContractMyPage.kt
@@ -5,7 +5,7 @@ import com.example.metaler_android.BaseView
 import com.example.metaler_android.data.profile.Profile
 import com.example.metaler_android.util.TapBarContract
 
-interface ContractHome {
+interface ContractMyPage {
     interface View : BaseView<Presenter>, TapBarContract.View {
         fun showProfile(profile: Profile)
 
