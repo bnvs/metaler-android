1aa0de6c8335372c332def48b68e378874354337
ohseungyeon <sally37345>
2020-03-24 20:42:03 +0900

    [ADD] PresenterMyPage.kt 상속 받은 인터페이스의 함수들 정의함

--- app/src/main/java/com/example/metaler_android/mypage/PresenterMyPage.kt
+++ app/src/main/java/com/example/metaler_android/mypage/PresenterMyPage.kt
@@ -1,2 +1,27 @@
 package com.example.metaler_android.mypage
 
+import android.content.Context
+
+class PresenterMyPage(context: Context, val view: ContractMyPage.View) : ContractMyPage.Presenter {
+
+    init {
+        view.presenter = this
+    }
+
+    override fun start() {
+        loadProfile()
+    }
+
+    override fun loadProfile(){
+
+    }
+
+    /**
+     * TapBarContract.Presenter 에서 상속받은 함수
+     * */
+    override fun openHome() { view.showHomeUi() }
+    override fun openMaterials() { view.showMaterialsUi() }
+    override fun openManufactures() { view.showManufacturesUi() }
+    override fun openBookmarks() { view.showBookmarksUi() }
+    override fun openMyPage() { view.showMyPageUi() }
+}
