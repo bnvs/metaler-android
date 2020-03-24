a46431f518d36af8eb0ad30b9aa8b883216d278f
ohseungyeon <sally37345>
2020-03-24 20:25:12 +0900

    [ADD] ContractMyPage.kt 뷰, 프레젠터에 필요한 기능 인터페이스에 추가함

--- app/src/main/java/com/example/metaler_android/mypage/ContractMyPage.kt
+++ app/src/main/java/com/example/metaler_android/mypage/ContractMyPage.kt
@@ -1,2 +1,18 @@
 package com.example.metaler_android.mypage
 
+import com.example.metaler_android.BasePresenter
+import com.example.metaler_android.BaseView
+import com.example.metaler_android.data.profile.Profile
+import com.example.metaler_android.util.TapBarContract
+
+interface ContractHome {
+    interface View : BaseView<Presenter>, TapBarContract.View {
+        fun showProfile(profile: Profile)
+
+    }
+
+    interface Presenter : BasePresenter, TapBarContract.Presenter {
+        fun loadProfile()
+
+    }
+}
