7a709c0951d99afa848ae6e08ed405b8b5a20928
nukeolaf <yhjin04170@gmail.com>
2020-03-25 01:26:12 +0900

    [RF] TapBarContract 의 View 인터페이스의 함수들을 default method 로 리팩토링

--- app/src/main/java/com/example/metaler_android/util/TapBarContract.kt
+++ app/src/main/java/com/example/metaler_android/util/TapBarContract.kt
@@ -1,17 +1,45 @@
 package com.example.metaler_android.util
 
+import android.content.Context
+import android.content.Intent
+import com.example.metaler_android.bookmark.ActivityBookmark
+import com.example.metaler_android.home.ActivityHome
+import com.example.metaler_android.manufactures.ActivityManufactures
+import com.example.metaler_android.materials.ActivityMaterials
+import com.example.metaler_android.mypage.ActivityMyPage
+
 interface TapBarContract {
 
     interface View {
-        fun showHomeUi()
+        fun showHomeUi(context: Context) {
+            Intent(context, ActivityHome::class.java).also {
+                context.startActivity(it)
+            }
+        }
 
-        fun showMaterialsUi()
+        fun showMaterialsUi(context: Context) {
+            Intent(context, ActivityMaterials::class.java).also {
+                context.startActivity(it)
+            }
+        }
 
-        fun showManufacturesUi()
+        fun showManufacturesUi(context: Context) {
+            Intent(context, ActivityManufactures::class.java).also {
+                context.startActivity(it)
+            }
+        }
 
-        fun showBookmarksUi()
+        fun showBookmarksUi(context: Context) {
+            Intent(context, ActivityBookmark::class.java).also {
+                context.startActivity(it)
+            }
+        }
 
-        fun showMyPageUi()
+        fun showMyPageUi(context: Context) {
+            Intent(context, ActivityMyPage::class.java).also {
+                context.startActivity(it)
+            }
+        }
     }
 
     interface Presenter {
