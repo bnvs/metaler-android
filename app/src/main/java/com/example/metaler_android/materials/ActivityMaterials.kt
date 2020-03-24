1156ee837b84e6bc23a69f04cbfc77638e99b0ae
nukeolaf <yhjin04170@gmail.com>
2020-03-25 01:47:06 +0900

    [RF] ActivityMaterials 의 TapBarContract.Presenter 함수 오버라이딩 코드 제거
    TapBarContract 의 Presenter 인터페이스 함수들을 default method 로 리팩토링하면서 오버라이딩 필요없어짐

--- app/src/main/java/com/example/metaler_android/materials/ActivityMaterials.kt
+++ app/src/main/java/com/example/metaler_android/materials/ActivityMaterials.kt
@@ -150,11 +150,11 @@ class ActivityMaterials : AppCompatActivity(), ContractMaterials.View {
     }
 
     private fun setTapBarButtons() {
-        homeBtn.setOnClickListener { presenter.openHome() }
-        materialsBtn.setOnClickListener { presenter.openMaterials() }
-        manufactureBtn.setOnClickListener { presenter.openManufactures() }
-        bookmarkBtn.setOnClickListener { presenter.openBookmarks() }
-        myPageBtn.setOnClickListener { presenter.openMyPage() }
+        homeBtn.setOnClickListener { presenter.openHome(this, this) }
+        materialsBtn.setOnClickListener { presenter.openMaterials(this, this) }
+        manufactureBtn.setOnClickListener { presenter.openManufactures(this, this) }
+        bookmarkBtn.setOnClickListener { presenter.openBookmarks(this, this) }
+        myPageBtn.setOnClickListener { presenter.openMyPage(this, this) }
     }
 
     /**
