a2c604cd3db119a9a2bf88c9da857279f1762180
ohseungyeon <sally37345>
2020-03-25 16:45:45 +0900

    [ADD] ActivityBookmarks.kt 에 클릭 이벤트 메소드 추가 및 onCreate에 클릭이벤트, 프레젠터 초기화함

--- app/src/main/java/com/example/metaler_android/bookmarks/ActivityBookmarks.kt
+++ app/src/main/java/com/example/metaler_android/bookmarks/ActivityBookmarks.kt
@@ -3,6 +3,14 @@ package com.example.metaler_android.bookmarks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.metaler_android.R
+import com.example.metaler_android.home.PresenterHome
+import kotlinx.android.synthetic.main.activity_bookmark.*
+import kotlinx.android.synthetic.main.activity_bookmark.bookmarkBtn
+import kotlinx.android.synthetic.main.activity_bookmark.homeBtn
+import kotlinx.android.synthetic.main.activity_bookmark.manufactureBtn
+import kotlinx.android.synthetic.main.activity_bookmark.materialsBtn
+import kotlinx.android.synthetic.main.activity_bookmark.myPageBtn
+import kotlinx.android.synthetic.main.activity_home.*
 
 class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
 
@@ -13,6 +21,19 @@ class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmark)
+
+        // Create the presenter
+        presenter = PresenterBookmarks(
+            this@ActivityBookmarks,
+            this@ActivityBookmarks
+        )
+
+        initClickListeners()
+
+        presenter.run {
+            start()
+        }
+
     }
 
     override fun showPostDetailUi() {
@@ -30,4 +51,23 @@ class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
     override fun showBookmarkDeleteDialog() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    private fun initClickListeners() {
+        setCategoryButtons()
+        setTapBarButtons()
+    }
+
+    private fun setCategoryButtons(){
+        materialsCategoryBtn.setOnClickListener { presenter.openMaterialsList() }
+        manufactureCategoryBtn.setOnClickListener { presenter.openManufacturesList() }
+    }
+
+    private fun setTapBarButtons() {
+        homeBtn.setOnClickListener { presenter.openHome(this, this) }
+        materialsBtn.setOnClickListener { presenter.openMaterials(this, this) }
+        manufactureBtn.setOnClickListener { presenter.openManufactures(this, this) }
+        bookmarkBtn.setOnClickListener { presenter.openBookmarks(this, this) }
+        myPageBtn.setOnClickListener { presenter.openMyPage(this, this) }
+    }
+
 }
