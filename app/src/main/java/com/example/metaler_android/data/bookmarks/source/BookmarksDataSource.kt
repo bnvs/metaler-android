823cc717ce065033d90105d0d68a3531aff171fb
ohseungyeon <sally37345>
2020-03-25 16:17:26 +0900

    [ADD] bookmarks 모델에 북마크 제거 할 때 필요한 코드 추가함

--- app/src/main/java/com/example/metaler_android/data/bookmarks/source/BookmarksDataSource.kt
+++ app/src/main/java/com/example/metaler_android/data/bookmarks/source/BookmarksDataSource.kt
@@ -1,2 +1,9 @@
 package com.example.metaler_android.data.bookmarks.source
 
+interface BookmarksDataSource{
+    interface DeleteBookmarkCallback {
+        fun onBookmarkDeleted()
+        fun onDataNotAvailable()
+    }
+    fun deleteBookmark(callback: DeleteBookmarkCallback)
+}
