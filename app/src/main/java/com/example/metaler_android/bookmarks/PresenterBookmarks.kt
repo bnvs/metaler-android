1446841bbb1dc5a27edb0d4adb4f47abeb304f34
ohseungyeon <sally37345>
2020-03-25 16:37:52 +0900

    [ADD] PresenterBookmarks.kt 에 ContractBookmarks 의 프레젠터 인터페이스에서 상속받은 함수 추가함

--- app/src/main/java/com/example/metaler_android/bookmarks/PresenterBookmarks.kt
+++ app/src/main/java/com/example/metaler_android/bookmarks/PresenterBookmarks.kt
@@ -1,2 +1,40 @@
 package com.example.metaler_android.bookmarks
 
+import android.content.Context
+
+class PresenterBookmarks (
+    private val context: Context,
+    private val view: ContractBookmarks.View) : ContractBookmarks.Presenter {
+
+    override fun loadMaterialsPost() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadManufacturePost() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openMaterialsList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openManufacturesList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openPostDetail(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openBookmarkDelete() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteBookmark() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
