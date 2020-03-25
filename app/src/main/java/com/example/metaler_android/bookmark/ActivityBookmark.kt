f513f0f38ab67db22a431bb181309ae4fddfa95b
ohseungyeon <sally37345>
2020-03-25 16:31:10 +0900

    [ADD] ActivityBookmark에 ContractBookmark 의 뷰 인터페이스에서 상속받은 함수 추가함

--- app/src/main/java/com/example/metaler_android/bookmark/ActivityBookmark.kt
+++ app/src/main/java/com/example/metaler_android/bookmark/ActivityBookmark.kt
@@ -4,10 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.metaler_android.R
 
-class ActivityBookmark : AppCompatActivity() {
+class ActivityBookmark : AppCompatActivity(), ContractBookmark.View {
+
+    private val TAG = "ActivityBookmark"
+
+    override lateinit var presenter: ContractBookmark.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmark)
+    }
+
+    override fun showPostDetailUi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showMaterialsList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showManufacturesList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showBookmarkDeleteDialog() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
