ab64c06f0b3424a1209f0935b44b96185a50a5f2
ohseungyeon <sally37345>
2020-03-25 17:40:22 +0900

    [MD] ActivityBookmarks.kt 에 리사이클러뷰 아이템 클릭리스너 주석 수정함

--- app/src/main/java/com/example/metaler_android/bookmarks/ActivityBookmarks.kt
+++ app/src/main/java/com/example/metaler_android/bookmarks/ActivityBookmarks.kt
@@ -20,7 +20,7 @@ class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
 
 
     /**
-     * 북마크 재료 카테고리 눌렀을 때 보여지는 재료 게시물 리사이클러뷰 아이템에 달아줄 클릭리스너입니다
+     * 북마크 재료/가공 카테고리 눌렀을 때 보여지는 재료/가공 게시물 리사이클러뷰 아이템에 달아줄 클릭리스너입니다
      * 아이템 클릭 시, 클릭한 게시물의 post_id 를 presenter 에 전달합니다.
      * */
     private var itemListener: PostItemListener = object : PostItemListener {
