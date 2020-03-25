54e64be6231f881df300d112c17de9dee3517b9d
ohseungyeon <sally37345>
2020-03-25 16:16:29 +0900

    [ADD] ContractBookmark 에서 bookmark 뷰, 프레젠터에서 필요한 함수들 정의함.

--- app/src/main/java/com/example/metaler_android/bookmark/ContractBookmark.kt
+++ app/src/main/java/com/example/metaler_android/bookmark/ContractBookmark.kt
@@ -1,2 +1,35 @@
 package com.example.metaler_android.bookmark
 
+import com.example.metaler_android.BasePresenter
+import com.example.metaler_android.BaseView
+import com.example.metaler_android.util.TapBarContract
+
+interface ContractBookmark {
+    interface View : BaseView<Presenter>, TapBarContract.View {
+        fun showPostDetailUi()
+
+        fun showMaterialsList()
+
+        fun showManufacturesList()
+
+        fun showBookmarkDeleteDialog()
+
+    }
+
+    interface Presenter : BasePresenter, TapBarContract.Presenter {
+        fun loadMaterialsPost()
+
+        fun loadManufacturePost()
+
+        fun openMaterialsList()
+
+        fun openManufacturesList()
+
+        fun openPostDetail(postId: Int)
+
+        fun openBookmarkDelete()
+
+        fun deleteBookmark()
+
+    }
+}
