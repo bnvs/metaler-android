8e2e1ce020c77fa114569f810bdc9bb7a10cbf28
ohseungyeon <sally37345>
2020-03-27 03:06:56 +0900

    [ADD] ActivityDetail.kt 상속받은 인터페이스의 함수들을 추가함

--- app/src/main/java/com/bnvs/metaler/detail/ActivityDetail.kt
+++ app/src/main/java/com/bnvs/metaler/detail/ActivityDetail.kt
@@ -6,10 +6,28 @@ import com.bnvs.metaler.R
 
 
 //재료탭, 가공탭의 상세페이지 레이아웃이 같기 떄문에 같이 사용한다.
-class ActivityDetail : AppCompatActivity() {
+class ActivityDetail : AppCompatActivity(), ContractDetail.View {
+
+    private val TAG = "ActivityDetail"
+
+    override lateinit var presenter: ContractDetail.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
     }
+
+    override fun showPostDetail() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showComments() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showMenuDialog() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
