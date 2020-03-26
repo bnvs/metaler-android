8e6a5050a2104a2225dfc386258ba70f5dff7e81
ohseungyeon <sally37345>
2020-03-26 23:51:01 +0900

    [FIX] bookmarks 모델 부분 머지하면서 코드 없어져서 다시 추가함

--- app/src/main/java/com/bnvs/metaler/data/bookmarks/source/BookmarksDataSource.kt
+++ app/src/main/java/com/bnvs/metaler/data/bookmarks/source/BookmarksDataSource.kt
@@ -1,2 +1,9 @@
 package com.bnvs.metaler.data.bookmarks.source
 
+interface BookmarksDataSource{
+    interface DeleteBookmarkCallback {
+        fun onBookmarkDeleted()
+        fun onDataNotAvailable()
+    }
+    fun deleteBookmark(callback: DeleteBookmarkCallback)
+}
--- app/src/main/java/com/bnvs/metaler/data/bookmarks/source/BookmarksRepository.kt
+++ app/src/main/java/com/bnvs/metaler/data/bookmarks/source/BookmarksRepository.kt
@@ -1,2 +1,13 @@
 package com.bnvs.metaler.data.bookmarks.source
 
+import android.content.Context
+import com.bnvs.metaler.data.bookmarks.source.remote.BookmarksRemoteDataSource
+
+class BookmarksRepository(context: Context) : BookmarksDataSource {
+
+    private val bookmarksRemoteDataSource = BookmarksRemoteDataSource
+
+    override fun deleteBookmark(callback: BookmarksDataSource.DeleteBookmarkCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
