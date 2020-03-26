c2df122032da3c8d6656b49055f495613959c2e0
ohseungyeon <sally37345>
2020-03-27 02:59:07 +0900

    [ADD] ContractDetail.kt 에 상세페이지에서 필요한 기능 정의함

--- app/src/main/java/com/bnvs/metaler/detail/ContractDetail.kt
+++ app/src/main/java/com/bnvs/metaler/detail/ContractDetail.kt
@@ -1,11 +1,31 @@
 package com.bnvs.metaler.detail
 
+import com.bnvs.metaler.BasePresenter
+import com.bnvs.metaler.BaseView
+
 interface ContractDetail {
-    interface View {
-        //
+    interface View : BaseView<Presenter> {
+        fun showPost()
+
+        fun showComments()
+
+        fun showMenuDialog()
+
     }
 
-    interface Presenter {
-        //
+    interface Presenter : BasePresenter {
+        fun loadPost()
+
+        fun loadComments()
+
+        fun addBookmark(postId: Int)
+
+        fun deleteBookmark(postId: Int)
+
+        fun openMenu()
+
+        fun deletePost()
+
+        fun modifyPost()
     }
 }
