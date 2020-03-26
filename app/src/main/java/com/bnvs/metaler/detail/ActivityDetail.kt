96a364c4d11d844f2ae42fca3bdb2246a432a495
ohseungyeon <sally37345>
2020-03-27 03:41:00 +0900

    [ADD] ActivityDetail.kt 에 클릭리스너 함수만 추가 및 onCreate에 클릭리스너 초기화, 프레젠터 초기화함

--- app/src/main/java/com/bnvs/metaler/detail/ActivityDetail.kt
+++ app/src/main/java/com/bnvs/metaler/detail/ActivityDetail.kt
@@ -3,6 +3,7 @@ package com.bnvs.metaler.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bnvs.metaler.R
+import kotlinx.android.synthetic.main.activity_detail.*
 
 
 //재료탭, 가공탭의 상세페이지 레이아웃이 같기 떄문에 같이 사용한다.
@@ -15,6 +16,17 @@ class ActivityDetail : AppCompatActivity(), ContractDetail.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        // Create the presenter
+        presenter = PresenterDetail(this, this)
+
+        // Set up Buttons
+        initClickListeners()
+
+        // 재료 탭 presenter 시작
+        presenter.run {
+            start()
+        }
     }
 
     override fun showPostDetail() {
@@ -28,6 +40,23 @@ class ActivityDetail : AppCompatActivity(), ContractDetail.View {
     override fun showMenuDialog() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+
+    private fun initClickListeners() {
+        setTitleBarButtons()
+        setRatingButtons()
+    }
+
+    private fun setTitleBarButtons() {
+        // 백버튼, 북마크, more 버튼 클릭 리스너 달아주기
+
+    }
+
+    private fun setRatingButtons() {
+        // 좋아요, 싫어요 버튼 클릭 리스너 달아주기
+
+    }
+
 
 
 }
