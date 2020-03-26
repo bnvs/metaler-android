ddb5dc63757a2bb717c5185718b6d75027534fc9
ohseungyeon <sally37345>
2020-03-27 03:09:15 +0900

    [ADD] PresenterDetail.kt에 상속받은 인터페이스의 함수들 추가함

--- app/src/main/java/com/bnvs/metaler/detail/PresenterDetail.kt
+++ app/src/main/java/com/bnvs/metaler/detail/PresenterDetail.kt
@@ -1,5 +1,45 @@
 package com.bnvs.metaler.detail
 
-class PresenterDetail {
-    //
+import android.content.Context
+
+class PresenterDetail(
+    private val context: Context,
+    private val view: ContractDetail.View) : ContractDetail.Presenter {
+
+    init {
+        view.presenter = this
+    }
+
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadPostDetail() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadComments() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addBookmark(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteBookmark(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openMenu() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deletePost(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun modifyPost(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
