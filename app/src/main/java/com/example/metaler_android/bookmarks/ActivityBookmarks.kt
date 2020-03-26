70500fa6d1e0a8bd2469c4d3d9ff4cee603db66a
ohseungyeon <sally37345>
2020-03-26 15:07:17 +0900

    [ADD] ActivityBookmarks.kt 의 리사이클러뷰 아이템 레이아웃이 PostAdapter 에서 inflate해서 사용하는 레이아웃과 달라서 리사이클러뷰 어댑터 커스텀함

--- app/src/main/java/com/example/metaler_android/bookmarks/ActivityBookmarks.kt
+++ app/src/main/java/com/example/metaler_android/bookmarks/ActivityBookmarks.kt
@@ -2,8 +2,16 @@ package com.example.metaler_android.bookmarks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.metaler_android.R
+import com.example.metaler_android.data.post.Post
 import com.example.metaler_android.home.PresenterHome
+import com.example.metaler_android.util.PostAdapter
 import kotlinx.android.synthetic.main.activity_bookmark.*
 import kotlinx.android.synthetic.main.activity_bookmark.bookmarkBtn
 import kotlinx.android.synthetic.main.activity_bookmark.homeBtn
@@ -11,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_bookmark.manufactureBtn
 import kotlinx.android.synthetic.main.activity_bookmark.materialsBtn
 import kotlinx.android.synthetic.main.activity_bookmark.myPageBtn
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.item_bookmark_rv.view.*
 
 class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
 
@@ -23,11 +32,19 @@ class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
      * 북마크 재료/가공 카테고리 눌렀을 때 보여지는 재료/가공 게시물 리사이클러뷰 아이템에 달아줄 클릭리스너입니다
      * 아이템 클릭 시, 클릭한 게시물의 post_id 를 presenter 에 전달합니다.
      * */
-    private var itemListener: PostItemListener = object : PostItemListener {
+    private var bookmarkItemListener: BookmarkPostItemListener = object : BookmarkPostItemListener {
         override fun onPostClick(clickedPostId: Int) {
             presenter.openPostDetail(clickedPostId)
         }
+
+        override fun onMoreButtonClick(view: View, clickedPostId: Int, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
+    private val bookmarkPostAdapter = BookmarkPostAdapter(ArrayList(0), bookmarkItemListener)
+    private val bookmarkPostLayoutManager = LinearLayoutManager(this)
+
+
 
 
 
@@ -85,8 +102,67 @@ class ActivityBookmarks : AppCompatActivity(), ContractBookmarks.View {
     }
 
 
-    private interface PostItemListener {
+
+    //북마크 리사이클러뷰 아이템 레이아웃(북마크 버튼 대신 delete 버튼있음)이 PostAdapter에서 쓰는 레이아웃이랑 달라서 일부 바꿈
+    private class BookmarkPostAdapter(
+        private var posts: List<Post>,
+        private val itemListener: BookmarkPostItemListener
+    ) : RecyclerView.Adapter<BookmarkPostAdapter.ViewHolder>() {
+
+        fun setPosts(list: List<Post>) {
+            this.posts = list
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val inflatedView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_bookmark_rv, parent, false)
+            return ViewHolder(inflatedView)
+        }
+
+        override fun getItemCount(): Int {
+            return posts.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.bind(posts[position], position)
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private var view: View = itemView
+
+            fun bind(item: Post, position: Int) {
+
+                var tags = ""
+                for (tag in item.tags) {
+                    tags += "#$tag "
+                }
+
+                view.apply {
+                    title.text = item.title
+                    userName.text = item.nickname
+                    date.text = item.date
+//                    tag.text = item.tags
+                    dislikeNum.text = item.dis_like.toString()
+                    likeNum.text = item.like.toString()
+                    setOnClickListener { itemListener.onPostClick(item.post_id) }
+                    moreBtn.setOnClickListener {
+                        itemListener.onMoreButtonClick(view, item.post_id,position)
+                    }
+
+                }
+
+                Glide.with(view)
+                    .load(item.attach_url)
+                    .into(view.img)
+            }
+        }
+    }
+
+
+    private interface BookmarkPostItemListener {
         fun onPostClick(clickedPostId: Int)
+        fun onMoreButtonClick(view: View, clickedPostId: Int, position: Int)
+
     }
 
 }
