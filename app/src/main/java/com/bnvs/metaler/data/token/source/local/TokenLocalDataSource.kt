e1e84dbdc2bcfcba2d1965576a3d3a144ec13c18
nukeolaf <yhjin04170@gmail.com>
2020-03-30 00:08:01 +0900

    [ADD] TokenDataSource, TokenLocalDataSource 추가, TokenRepository 추가

---
+++ app/src/main/java/com/bnvs/metaler/data/token/source/local/TokenLocalDataSource.kt
@@ -0,0 +1,40 @@
+package com.bnvs.metaler.data.token.source.local
+
+import android.content.Context
+import com.bnvs.metaler.data.token.AccessToken
+import com.bnvs.metaler.data.token.SigninToken
+import com.bnvs.metaler.data.token.source.TokenDataSource
+
+class TokenLocalDataSource (context: Context) : TokenDataSource {
+
+    private val sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE)
+    private val editor = sharedPreferences.edit()
+
+    override fun getSigninToken(callback: TokenDataSource.LoadSigninTokenCallback) {
+        var token = sharedPreferences.getString("signin_token", null)
+        if (token != null) {
+            callback.onTokenloaded(SigninToken(token))
+        }else {
+            callback.onTokenNotExist()
+        }
+    }
+
+    override fun saveSigninToken(token: SigninToken) {
+        editor.putString("signin_token", token.signin_token)
+        editor.commit()
+    }
+
+    override fun getAccessToken(callback: TokenDataSource.LoadAccessTokenCallback) {
+        var token = sharedPreferences.getString("access_token", null)
+        if (token != null) {
+            callback.onTokenloaded(AccessToken(token))
+        }else {
+            callback.onTokenNotExist()
+        }
+    }
+
+    override fun saveAccessToken(token: AccessToken) {
+        editor.putString("signin_token", token.access_token)
+        editor.commit()
+    }
+}
