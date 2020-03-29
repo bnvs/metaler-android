4dc9132c3b20471f5519502a654c16ca3dbfa261
nukeolaf <yhjin04170@gmail.com>
2020-03-29 16:35:52 +0900

    [ADD] ActivityLogin 에 ISessionCallback 상속받은 SessionCallback 클래스 작성

---
+++ app/src/main/java/com/bnvs/metaler/login/ActivityLogin.kt
@@ -0,0 +1,49 @@
+package com.bnvs.metaler.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.bnvs.metaler.R
+import com.kakao.auth.ISessionCallback
+import com.kakao.network.ErrorResult
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.MeV2ResponseCallback
+import com.kakao.usermgmt.response.MeV2Response
+import com.kakao.util.exception.KakaoException
+
+class ActivityLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+    private inner class SessionCallback : ISessionCallback {
+        override fun onSessionOpened() {
+            // 로그인 세션이 열렸을 때
+            UserManagement.getInstance().me( object : MeV2ResponseCallback() {
+                override fun onSuccess(result: MeV2Response?) {
+                    // 로그인이 성공했을 때
+                }
+
+                override fun onSessionClosed(errorResult: ErrorResult?) {
+                    // 로그인 도중 세션이 비정상적인 이유로 닫혔을 때
+                }
+            })
+        }
+        override fun onSessionOpenFailed(exception: KakaoException?) {
+            // 로그인 세션이 정상적으로 열리지 않았을 때
+            if (exception != null) {
+                com.kakao.util.helper.log.Logger.e(exception)
+                Toast.makeText(
+                    this@ActivityLogin,
+                    "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요 : $exception",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+}
