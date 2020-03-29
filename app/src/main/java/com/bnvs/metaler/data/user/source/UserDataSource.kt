1b4a0495dcf9bb8428dd6c93224df541037f255d
nukeolaf <yhjin04170@gmail.com>
2020-03-30 04:26:54 +0900

    [ADD] UserDataSource 인터페이스 추가
    회원가입, 회원탈퇴, 회원가입여부 확인, 로그인 기능

--- app/src/main/java/com/bnvs/metaler/data/user/source/UserDataSource.kt
+++ app/src/main/java/com/bnvs/metaler/data/user/source/UserDataSource.kt
@@ -1,14 +1,37 @@
 package com.bnvs.metaler.data.user.source
 
-import org.json.JSONObject
+import com.bnvs.metaler.data.user.AddUserResponse
+import com.bnvs.metaler.data.user.CheckMembershipResponse
+import com.bnvs.metaler.data.user.DeleteUserRequest
+import com.bnvs.metaler.data.user.LoginResponse
 
 interface UserDataSource {
 
     interface AddUserCallback {
-        fun onUserAdded(user: JSONObject)
+        fun onUserAdded(user: AddUserResponse)
+        fun onFailure()
+    }
+
+    interface DeleteUserCallback {
+        fun onUserDeleted(user: DeleteUserRequest)
+        fun onFailure()
+    }
+
+    interface CheckMembershipCallback {
+        fun onMembershipChecked(user: CheckMembershipResponse)
+        fun onResponseError(message: String)
+        fun onFailure(t: Throwable)
+    }
+
+    interface LoginCallback {
+        fun onLoginSuccess(user: LoginResponse)
         fun onFailure()
     }
 
     fun addUser(callback: AddUserCallback)
+    fun deleteUser(callback: DeleteUserCallback)
 
+    fun checkMembership(kakao_id: String, callback: CheckMembershipCallback)
+
+    fun login(callback: LoginCallback)
 }
