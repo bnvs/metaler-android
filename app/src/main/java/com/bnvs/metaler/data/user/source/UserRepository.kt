184a32f2ad7574dda37bbb822a7446eea57ba0e9
nukeolaf <yhjin04170@gmail.com>
2020-03-30 04:30:40 +0900

    [ADD] UserRepository class 작성

--- app/src/main/java/com/bnvs/metaler/data/user/source/UserRepository.kt
+++ app/src/main/java/com/bnvs/metaler/data/user/source/UserRepository.kt
@@ -1,5 +1,27 @@
 package com.bnvs.metaler.data.user.source
 
-object UserRepository {
-    //
+import com.bnvs.metaler.data.user.source.remote.UserRemoteDataSource
+
+class UserRepository : UserDataSource{
+
+    private val userRemoteDataSource = UserRemoteDataSource
+
+    override fun addUser(callback: UserDataSource.AddUserCallback) {
+        userRemoteDataSource.addUser(callback)
+    }
+
+    override fun deleteUser(callback: UserDataSource.DeleteUserCallback) {
+        userRemoteDataSource.deleteUser(callback)
+    }
+
+    override fun checkMembership(
+        kakao_id: String,
+        callback: UserDataSource.CheckMembershipCallback
+    ) {
+        userRemoteDataSource.checkMembership(kakao_id, callback)
+    }
+
+    override fun login(callback: UserDataSource.LoginCallback) {
+        userRemoteDataSource.login(callback)
+    }
 }
