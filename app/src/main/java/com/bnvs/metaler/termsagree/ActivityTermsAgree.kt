e5bfd766fe56793d5b800cb4c36491f96e4fd31a
nukeolaf <yhjin04170@gmail.com>
2020-03-31 23:04:45 +0900

    [ADD] 약관 동의 체크박스 기능 구현

--- app/src/main/java/com/bnvs/metaler/termsagree/ActivityTermsAgree.kt
+++ app/src/main/java/com/bnvs/metaler/termsagree/ActivityTermsAgree.kt
@@ -2,12 +2,46 @@ package com.bnvs.metaler.termsagree
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
 import com.bnvs.metaler.R
+import kotlinx.android.synthetic.main.activity_terms_agree.*
 
 class ActivityTermsAgree : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_agree)
+
+        allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
+        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
+        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
+        thirdCheckBtn.setOnClickListener { onCheckChanged(thirdCheckBtn) }
+        fourthCheckBtn.setOnClickListener { onCheckChanged(fourthCheckBtn) }
+
+    }
+
+    private fun onCheckChanged(compoundButton: CompoundButton) {
+        when(compoundButton.id) {
+            R.id.allCheckBtn -> {
+                if (allCheckBtn.isChecked) {
+                    firstCheckBtn.isChecked = true
+                    secondCheckBtn.isChecked = true
+                    thirdCheckBtn.isChecked = true
+                    fourthCheckBtn.isChecked = true
+                }else {
+                    firstCheckBtn.isChecked = false
+                    secondCheckBtn.isChecked = false
+                    thirdCheckBtn.isChecked = false
+                    fourthCheckBtn.isChecked = false
+                }
+            }
+            else -> {
+                allCheckBtn.isChecked = (
+                        firstCheckBtn.isChecked
+                        && secondCheckBtn.isChecked
+                        && thirdCheckBtn.isChecked
+                        && fourthCheckBtn.isChecked)
+            }
+        }
     }
 }
