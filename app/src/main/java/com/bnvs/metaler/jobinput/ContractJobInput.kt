3da08d764b61194815dba68d332dbf0702624ff3
nukeolaf <yhjin04170@gmail.com>
2020-04-01 00:37:53 +0900

    [ADD] ContractJobInput 파일 추가

---
+++ app/src/main/java/com/bnvs/metaler/jobinput/ContractJobInput.kt
@@ -0,0 +1,14 @@
+package com.bnvs.metaler.jobinput
+
+import com.bnvs.metaler.BasePresenter
+import com.bnvs.metaler.BaseView
+
+interface ContractJobInput {
+    interface View: BaseView<Presenter> {
+
+    }
+
+    interface Presenter: BasePresenter {
+
+    }
+}
