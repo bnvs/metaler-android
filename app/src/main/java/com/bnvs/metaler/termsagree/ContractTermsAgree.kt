3dd6bdbe7020f516f49f15086a04ebaf00a65aab
nukeolaf <yhjin04170@gmail.com>
2020-03-31 19:22:36 +0900

    [ADD] ContractTermsAgree 에 View, Presenter interface 함수 작성

--- app/src/main/java/com/bnvs/metaler/termsagree/ContractTermsAgree.kt
+++ app/src/main/java/com/bnvs/metaler/termsagree/ContractTermsAgree.kt
@@ -5,9 +5,19 @@ import com.bnvs.metaler.BaseView
 
 interface ContractTermsAgree {
     interface View : BaseView<Presenter> {
+        fun showTermsWebView()
 
+        fun enableCheckButton()
+
+        fun disableCheckButton()
+
+        fun showAgreeTermsDialog()
     }
     interface Presenter : BasePresenter {
+        fun clickAgreeButton()
 
+        fun clickAgreeMoreButton()
+
+        fun clickNextButton()
     }
 }
