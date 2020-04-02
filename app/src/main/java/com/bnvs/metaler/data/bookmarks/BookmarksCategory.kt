f8498e87fa57d63aee0b0c51b4cb8cbfbf714932
nukeolaf <yhjin04170@gmail.com>
2020-04-02 21:53:59 +0900

    [RM] 북마크 관련 필요없는 data class 삭제

--- app/src/main/java/com/bnvs/metaler/data/bookmarks/BookmarksCategory.kt
+++
@@ -1,9 +0,0 @@
-package com.bnvs.metaler.data.bookmarks
-
-data class BookmarksCategory(
-    val type: String,
-    val page: Int,
-    val limit: Int,
-    val search_type: String,
-    val search_word: String
-)
