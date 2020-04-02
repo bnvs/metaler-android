47237e9b8028195b4ff0a98437af22577f5e6e52
nukeolaf <yhjin04170@gmail.com>
2020-04-02 17:50:07 +0900

    [MD] PostsRequest data class api 에 맞게 수정

--- app/src/main/java/com/bnvs/metaler/data/posts/PostsRequest.kt
+++ app/src/main/java/com/bnvs/metaler/data/posts/PostsRequest.kt
@@ -1,6 +1,9 @@
 package com.bnvs.metaler.data.posts
 
 data class PostsRequest(
-    val access_token: String,
-    val category: List<PostsCategory>
+    val category_id: Int,
+    val page: Int,
+    val limit: Int,
+    val search_type: String,
+    val search_word: String
 )
