05c83d904bc13b578a18b40026731a3c59067a4a
nukeolaf <yhjin04170@gmail.com>
2020-04-02 18:04:45 +0900

    [MD] 게시글 목록 조회 request 에서 search_type 과 search_word 를 null 값으로 받을 수 있도록 수정ㅇ

--- app/src/main/java/com/bnvs/metaler/data/posts/PostsRequest.kt
+++ app/src/main/java/com/bnvs/metaler/data/posts/PostsRequest.kt
@@ -4,6 +4,6 @@ data class PostsRequest(
     val category_id: Int,
     val page: Int,
     val limit: Int,
-    val search_type: String,
-    val search_word: String
+    val search_type: String?,
+    val search_word: String?
 )
--- app/src/main/java/com/bnvs/metaler/data/posts/PostsResponse.kt
+++ app/src/main/java/com/bnvs/metaler/data/posts/PostsResponse.kt
@@ -3,5 +3,6 @@ package com.bnvs.metaler.data.posts
 data class PostsResponse(
     val count: Int,
     val is_next: Boolean,
-    val posts: List<Post>
+    val posts: List<Post>,
+    val code: String
 )
