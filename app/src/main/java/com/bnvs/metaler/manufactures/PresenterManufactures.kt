2ce47e8f4e5b19487ffb953e4ce630b99e1b3bad
ohseungyeon <sally37345>
2020-04-10 15:39:51 +0900

    [ADD] PresenterManufactures.kt 에 게시글 데이터 가져오는 loadPosts() 함수 모델과 연결함

--- app/src/main/java/com/bnvs/metaler/manufactures/PresenterManufactures.kt
+++ app/src/main/java/com/bnvs/metaler/manufactures/PresenterManufactures.kt
@@ -1,6 +1,8 @@
 package com.bnvs.metaler.manufactures
 
 import android.content.Context
+import com.bnvs.metaler.data.posts.PostsResponse
+import com.bnvs.metaler.data.posts.source.PostsDataSource
 import com.bnvs.metaler.data.posts.source.PostsRepository
 import com.bnvs.metaler.data.token.AccessToken
 import com.bnvs.metaler.data.token.source.TokenDataSource
@@ -11,11 +13,11 @@ class PresenterManufactures(
     val view: ContractManufactures.View
 ) : ContractManufactures.Presenter {
 
-    private val tokenRepository : TokenRepository = TokenRepository(context)
+    private val tokenRepository: TokenRepository = TokenRepository(context)
 
     private val postRepository: PostsRepository = PostsRepository(context)
 
-    private var accessToken : String = ""
+    private var accessToken: String = ""
 
     init {
         view.presenter = this
@@ -28,10 +30,11 @@ class PresenterManufactures(
     }
 
     override fun getAccessToken() {
-        tokenRepository.getAccessToken(object : TokenDataSource.LoadAccessTokenCallback{
+        tokenRepository.getAccessToken(object : TokenDataSource.LoadAccessTokenCallback {
             override fun onTokenloaded(token: AccessToken) {
                 accessToken = token.access_token //유효성 (24시간 지났는지) 검사
             }
+
             override fun onTokenNotExist() {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
@@ -39,8 +42,24 @@ class PresenterManufactures(
     }
 
     override fun loadPosts() {
+        postRepository.getPosts(
+            accessToken,
+            postsRequest,
+            object : PostsDataSource.LoadPostsCallback {
+                override fun onPostsLoaded(postsResponse: PostsResponse) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
 
+                override fun onResponseError(message: String) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onFailure(t: Throwable) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
     }
+
 
     override fun requestPosts() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
