34996e7edb5b54034c7eb46dbb31577511bc0398
nukeolaf <yhjin04170@gmail.com>
2020-04-12 15:11:10 +0900

    [ADD] 게시글작성 1단계 Contract 인터페이스 작성

---
+++ app/src/main/java/com/bnvs/metaler/postfirst/ContractPostFirst.kt
@@ -0,0 +1,19 @@
+package com.bnvs.metaler.postfirst
+
+import com.bnvs.metaler.BasePresenter
+import com.bnvs.metaler.BaseView
+
+interface ContractPostFirst {
+    interface View : BaseView<Presenter> {
+        fun showCategories()
+        fun showImageDeleteDialog()
+        fun showPostSecondUi()
+    }
+
+    interface Presenter : BasePresenter {
+        fun getImageFromLocal()
+        fun uploadImage()
+        fun getAttachUrl()
+        fun openPostSecond()
+    }
+}
