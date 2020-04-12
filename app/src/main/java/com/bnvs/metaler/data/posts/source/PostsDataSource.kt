79e83989e2eec378b13c5f8ae55b94079bd5b8fc
ohseungyeon <sally37345>
2020-04-12 23:06:48 +0900

    [ADD] PostsDataSource.kt onPostsLoaded 리턴타입 명시함

--- app/src/main/java/com/bnvs/metaler/data/posts/source/PostsDataSource.kt
+++ app/src/main/java/com/bnvs/metaler/data/posts/source/PostsDataSource.kt
@@ -1,12 +1,13 @@
 package com.bnvs.metaler.data.posts.source
 
+import com.bnvs.metaler.data.posts.Post
 import com.bnvs.metaler.data.posts.PostsRequest
 import com.bnvs.metaler.data.posts.PostsResponse
 
 interface PostsDataSource {
 
     interface LoadPostsCallback {
-        fun onPostsLoaded(postsResponse: PostsResponse)
+        fun onPostsLoaded(postsResponse: PostsResponse): List<Post>
         fun onResponseError(message: String)
         fun onFailure(t: Throwable)
     }
