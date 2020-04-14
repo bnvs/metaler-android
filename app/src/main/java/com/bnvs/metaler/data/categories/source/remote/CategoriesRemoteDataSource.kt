5c68cfbedcbe4391abe77932d9c61df95367e45f
nukeolaf <yhjin04170@gmail.com>
2020-04-15 06:50:55 +0900

    [ADD] 카테고리 목록 가져오는 모델 추가

--- app/src/main/java/com/bnvs/metaler/data/categories/source/remote/CategoriesRemoteDataSource.kt
+++ app/src/main/java/com/bnvs/metaler/data/categories/source/remote/CategoriesRemoteDataSource.kt
@@ -1,13 +1,34 @@
 package com.bnvs.metaler.data.categories.source.remote
 
+import com.bnvs.metaler.data.categories.Categories
 import com.bnvs.metaler.data.categories.source.CategoriesDataSource
 import com.bnvs.metaler.network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
 
 object CategoriesRemoteDataSource : CategoriesDataSource {
 
     private val retrofitClient = RetrofitClient.client
 
-    override fun getCatagories(callback: CategoriesDataSource.LoadCategoriesCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getCategories(
+        onSuccess: (response: Categories) -> Unit,
+        onFailure: (e: Throwable) -> Unit
+    ) {
+        retrofitClient.getCategories().enqueue(object : Callback<Categories> {
+            override fun onResponse(call: Call<Categories>, response: Response<Categories>) {
+                val body = response.body()
+                if (body != null && response.isSuccessful) {
+                    onSuccess(body)
+                } else {
+                    onFailure(HttpException(response))
+                }
+            }
+
+            override fun onFailure(call: Call<Categories>, t: Throwable) {
+                onFailure(t)
+            }
+        })
     }
 }
