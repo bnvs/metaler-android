4b84c0c879d3e62df03912d97f6765e923d00c74
nukeolaf <yhjin04170@gmail.com>
2020-04-15 03:33:10 +0900

    [ADD] NetworkUtil 에 e 가 HttpException 일 경우 파싱해서 메시지 반환해주는 코드 추가

--- app/src/main/java/com/bnvs/metaler/network/NetworkUtil.kt
+++ app/src/main/java/com/bnvs/metaler/network/NetworkUtil.kt
@@ -2,8 +2,22 @@ package com.bnvs.metaler.network
 
 import com.bnvs.metaler.data.error.ErrorResponse
 import okhttp3.ResponseBody
+import retrofit2.HttpException
 
 object NetworkUtil {
+    fun getErrorMessage(e: Throwable): String {
+        return if (e is HttpException) {
+            val errorBody = e.response().errorBody()
+            if (errorBody != null) {
+                getErrorResponse(errorBody).toString()
+            } else {
+                "${e.code()} ${e.message()}"
+            }
+        } else {
+            "$e"
+        }
+    }
+
     fun getErrorResponse(errorBody: ResponseBody): ErrorResponse {
         return RetrofitClient.retrofit.responseBodyConverter<ErrorResponse>(
             ErrorResponse::class.java,
