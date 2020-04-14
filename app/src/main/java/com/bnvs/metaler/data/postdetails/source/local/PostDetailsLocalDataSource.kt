facf024fd1ccfccedec201e5f9e8c351a15f9d04
nukeolaf <yhjin04170@gmail.com>
2020-04-15 06:54:42 +0900

    [ADD] LocalDataSource SharedPreference 저장소 이름 수정

--- app/src/main/java/com/bnvs/metaler/data/postdetails/source/local/PostDetailsLocalDataSource.kt
+++ app/src/main/java/com/bnvs/metaler/data/postdetails/source/local/PostDetailsLocalDataSource.kt
@@ -1,14 +1,20 @@
 package com.bnvs.metaler.data.postdetails.source.local
 
 import android.content.Context
+import com.bnvs.metaler.data.postdetails.PostDetails
 import com.bnvs.metaler.data.postdetails.source.PostDetailsDataSource
 
 class PostDetailsLocalDataSource(context: Context) : PostDetailsDataSource {
 
-    private val sharedPreferences = context.getSharedPreferences("postDetails", Context.MODE_PRIVATE)
+    private val sharedPreferences =
+        context.getSharedPreferences("postDetails", Context.MODE_PRIVATE)
     private val editor = sharedPreferences.edit()
 
-    override fun getPostDetails(callback: PostDetailsDataSource.LoadPostDetailsCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getPostDetails(
+        postId: Int,
+        onSuccess: (response: PostDetails) -> Unit,
+        onFailure: (e: Throwable) -> Unit
+    ) {
+
     }
 }
