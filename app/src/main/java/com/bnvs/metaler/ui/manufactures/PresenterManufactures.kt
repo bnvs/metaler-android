9886c7375b9a21663874bd3f3d7b87a4ebb47210
ohseungyeon <sally37345>
2020-04-17 19:01:22 +0900

    [RF] PresenterManufactures.kt loadPosts함수 고차함수로 수정함

--- app/src/main/java/com/bnvs/metaler/ui/manufactures/PresenterManufactures.kt
+++ app/src/main/java/com/bnvs/metaler/ui/manufactures/PresenterManufactures.kt
@@ -1,18 +1,19 @@
 package com.bnvs.metaler.ui.manufactures
 
 import android.content.Context
-import android.util.Log
+import android.widget.Toast
 import com.bnvs.metaler.data.posts.Post
 import com.bnvs.metaler.data.posts.PostsRequest
 import com.bnvs.metaler.data.posts.PostsResponse
-import com.bnvs.metaler.data.posts.source.PostsDataSource
 import com.bnvs.metaler.data.posts.source.PostsRepository
 import com.bnvs.metaler.data.token.source.TokenRepository
+import com.bnvs.metaler.network.NetworkUtil
 
 class PresenterManufactures(
     private val context: Context,
     val view: ContractManufactures.View
 ) : ContractManufactures.Presenter {
+
 
     val TAG = "PresenterManufactures.kt"
 
@@ -52,47 +53,45 @@ class PresenterManufactures(
 
     override fun loadPosts(postsRequest: PostsRequest) {
         postRepository.getPosts(
-            accessToken,
             postsRequest,
-            object : PostsDataSource.LoadPostsCallback {
-                override fun onPostsLoaded(postsResponse: PostsResponse) {
-                    view.showPosts(postsResponse.posts)
-                }
-
-                override fun onResponseError(message: String) {
-                    Log.d(TAG, "message ? : $message")
-                }
-
-                override fun onFailure(t: Throwable) {
-                    Log.d(TAG, "t ? : $t")
-                }
-            })
+            onSuccess = { response: PostsResponse ->
+                view.showPosts(response.posts) },
+            onFailure = { e ->
+                Toast.makeText(context,"서버 통신 실패 : ${NetworkUtil.getErrorMessage(e)}",Toast.LENGTH_LONG).show()
+            }
+        )
     }
 
     override fun loadMorePosts(postsRequest: PostsRequest) {
-        postRepository.getPosts(
-            accessToken,
-            postsRequest,
-            object : PostsDataSource.LoadPostsCallback {
-                override fun onPostsLoaded(postsResponse: PostsResponse) {
-                    if(postsResponse.is_next)
-                    view.showMorePosts(postsResponse.posts)
-                }
-
-                override fun onResponseError(message: String) {
-                    Log.d(TAG, "message ? : $message")
-                }
-
-                override fun onFailure(t: Throwable) {
-                    Log.d(TAG, "t ? : $t")
-                }
-            })
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+
+//    override fun loadMorePosts(postsRequest: PostsRequest) {
+//        postRepository.getPosts(
+//            accessToken,
+//            postsRequest,
+//            object : PostsDataSource.LoadPostsCallback {
+//                override fun onPostsLoaded(postsResponse: PostsResponse) {
+//                    Log.d(TAG, "loadMorePosts() 실행! ")
+//                    if(postsResponse.is_next)
+//                        view.getMorePosts(postsResponse.posts)
+//                }
+//
+//                override fun onResponseError(message: String) {
+//                    Log.d(TAG, "message ? : $message")
+//                }
+//
+//                override fun onFailure(t: Throwable) {
+//                    Log.d(TAG, "t ? : $t")
+//                }
+//            })
+//    }
 
 
     // getPosts api 요청 request body 반환하는 함수
     override fun requestPosts(): PostsRequest {
-        pageNum ++
+        pageNum++
         postsRequest = PostsRequest(
             1,
             pageNum,
@@ -102,7 +101,6 @@ class PresenterManufactures(
         )
         return postsRequest
     }
-
 
     override fun refreshPosts() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
