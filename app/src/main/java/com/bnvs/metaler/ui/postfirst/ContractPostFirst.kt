fb8d646be1f9df69066801f5de4073eb3814a356
nukeolaf <yhjin04170@gmail.com>
2020-04-19 13:30:14 +0900

    [ADD] 글작성 1단계 Contract 작성

--- app/src/main/java/com/bnvs/metaler/ui/postfirst/ContractPostFirst.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postfirst/ContractPostFirst.kt
@@ -7,11 +7,16 @@ interface ContractPostFirst {
     interface View : BaseView<Presenter> {
         fun showCategories()
         fun showImageDeleteDialog()
+        fun showEmptyTitleDialog()
+        fun showEmptyPriceDialog()
+        fun showEmptyPriceTypeDialog()
+        fun showEmptyContentsDialog()
         fun showPostSecondUi()
     }
 
     interface Presenter : BasePresenter {
         fun getImageFromLocal()
+        fun getImageFromCamera()
         fun uploadImage()
         fun getAttachUrl()
         fun openPostSecond()
