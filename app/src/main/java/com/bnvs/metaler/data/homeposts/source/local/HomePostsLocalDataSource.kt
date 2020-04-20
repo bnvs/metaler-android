8b9bd6610214a961e1ce34f2b12b641b6e09d572
ohseungyeon <sally37345>
2020-04-21 02:43:05 +0900

    [MD] HomePostsLocalDataSource.kt getHomePosts 고차함수로 변경함

--- app/src/main/java/com/bnvs/metaler/data/homeposts/source/local/HomePostsLocalDataSource.kt
+++ app/src/main/java/com/bnvs/metaler/data/homeposts/source/local/HomePostsLocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.bnvs.metaler.data.homeposts.source.local
 
 import android.content.Context
+import com.bnvs.metaler.data.homeposts.HomePosts
 import com.bnvs.metaler.data.homeposts.source.HomePostsDataSource
 
 class HomePostsLocalDataSource(context: Context) : HomePostsDataSource {
@@ -8,7 +9,10 @@ class HomePostsLocalDataSource(context: Context) : HomePostsDataSource {
     private val sharedPreferences = context.getSharedPreferences("homePosts", Context.MODE_PRIVATE)
     private val editor = sharedPreferences.edit()
 
-    override fun getHomePosts(callback: HomePostsDataSource.LoadHomePostsCallback) {
+    override fun getHomePosts(
+        onSuccess: (response: HomePosts) -> Unit,
+        onFailure: (e: Throwable) -> Unit
+    ) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
