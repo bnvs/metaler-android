16f0b5593f52b5ca02bdc71b44a8f83b47122337
ohseungyeon <sally37345>
2020-04-21 02:59:10 +0900

    [ADD] PresenterMaterials.kt 에 PresenterManufactures 참고해서 loadPosts, loadMorePosts, requestPosts 함수 추가함

--- app/src/main/java/com/bnvs/metaler/ui/materials/PresenterMaterials.kt
+++ app/src/main/java/com/bnvs/metaler/ui/materials/PresenterMaterials.kt
@@ -1,26 +1,93 @@
 package com.bnvs.metaler.ui.materials
 
 import android.content.Context
+import android.widget.Toast
+import com.bnvs.metaler.data.posts.Post
+import com.bnvs.metaler.data.posts.PostsRequest
+import com.bnvs.metaler.data.posts.PostsResponse
+import com.bnvs.metaler.data.posts.source.PostsRepository
+import com.bnvs.metaler.network.NetworkUtil
 
 class PresenterMaterials(
     private val context: Context,
     private val view: ContractMaterials.View
 ) : ContractMaterials.Presenter {
 
+    val TAG = "PresenterMatrials.kt"
+
+    private val postRepository: PostsRepository = PostsRepository(context)
+
+    private lateinit var postsRequest: PostsRequest
+
+    private var pageNum: Int = 0
+
+    lateinit var posts: List<Post>
+
     init {
         view.presenter = this
     }
 
     override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        loadPosts(requestPosts())
     }
 
     override fun loadCategories() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun loadPosts() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun loadPosts(postsRequest: PostsRequest) {
+        postRepository.getPosts(
+            postsRequest,
+            onSuccess = { response: PostsResponse ->
+                view.showPosts(response.posts)
+            },
+            onFailure = { e ->
+                Toast.makeText(
+                    context,
+                    "서버 통신 실패 : ${NetworkUtil.getErrorMessage(e)}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        )
+    }
+
+    override fun loadMorePosts(postsRequest: PostsRequest) {
+        postRepository.getPosts(
+            postsRequest,
+            onSuccess = { response: PostsResponse ->
+                if (response.is_next) {
+                    view.showMorePosts(response.posts)
+                } else {
+                    Toast.makeText(
+                        context,
+                        "마지막 아이템입니다.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            },
+            onFailure = { e ->
+                Toast.makeText(
+                    context,
+                    "서버 통신 실패 : ${NetworkUtil.getErrorMessage(e)}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        )
+    }
+
+
+    // getPosts api 요청 request body 반환하는 함수
+    override fun requestPosts(): PostsRequest {
+        pageNum++
+        postsRequest = PostsRequest(
+            1,
+            pageNum,
+            10,
+            null,
+            null
+        )
+        return postsRequest
     }
 
     override fun refreshPosts() {
