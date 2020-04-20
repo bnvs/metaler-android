2b2553a60aeab85f9ba683ef9ed6f60b693d3cf0
ohseungyeon <sally37345>
2020-04-21 02:43:42 +0900

    [ADD] AddEditPostRepository.kt 상속받은 클래스의 함수 추가함

--- app/src/main/java/com/bnvs/metaler/data/addeditpost/source/AddEditPostRepository.kt
+++ app/src/main/java/com/bnvs/metaler/data/addeditpost/source/AddEditPostRepository.kt
@@ -1,10 +1,8 @@
 package com.bnvs.metaler.data.addeditpost.source
 
-import com.bnvs.metaler.data.addeditpost.AddEditPostRequest
-import com.bnvs.metaler.data.addeditpost.AddPostResponse
-import com.bnvs.metaler.data.addeditpost.DeletePostResponse
-import com.bnvs.metaler.data.addeditpost.EditPostResponse
+import com.bnvs.metaler.data.addeditpost.*
 import com.bnvs.metaler.data.addeditpost.source.remote.AddEditPostRemoteDataSource
+import okhttp3.MultipartBody
 
 class AddEditPostRepository : AddEditPostDataSource {
 
@@ -33,5 +31,13 @@ class AddEditPostRepository : AddEditPostDataSource {
         onFailure: (e: Throwable) -> Unit
     ) {
         addeditPostRemoteDataSource.deletePost(postId, onSuccess, onFailure)
+    }
+
+    override fun uploadFile(
+        file: MultipartBody.Part,
+        onSuccess: (response: UploadFileResponse) -> Unit,
+        onFailure: (e: Throwable) -> Unit
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
