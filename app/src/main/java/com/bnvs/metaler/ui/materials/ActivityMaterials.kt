45f4f9ff0d121625703019a8334180e669197502
ohseungyeon <sally37345>
2020-04-21 02:54:26 +0900

    [ADD] ActivityMaterials.kt 에 showPosts, showMorePosts 함수 추가함

--- app/src/main/java/com/bnvs/metaler/ui/materials/ActivityMaterials.kt
+++ app/src/main/java/com/bnvs/metaler/ui/materials/ActivityMaterials.kt
@@ -1,6 +1,8 @@
 package com.bnvs.metaler.ui.materials
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +12,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bnvs.metaler.R
 import com.bnvs.metaler.data.categories.Category
+import com.bnvs.metaler.data.posts.Post
+import com.bnvs.metaler.util.EndlessRecyclerViewScrollListener
 import com.bnvs.metaler.util.PostAdapter
-import com.bnvs.metaler.util.PostItemListener
+import kotlinx.android.synthetic.main.activity_manufacture.*
 import kotlinx.android.synthetic.main.activity_materials.*
+import kotlinx.android.synthetic.main.activity_materials.bookmarkBtn
+import kotlinx.android.synthetic.main.activity_materials.homeBtn
+import kotlinx.android.synthetic.main.activity_materials.manufactureBtn
+import kotlinx.android.synthetic.main.activity_materials.materialsBtn
+import kotlinx.android.synthetic.main.activity_materials.myPageBtn
+import kotlinx.android.synthetic.main.activity_materials.postsRV
 import kotlinx.android.synthetic.main.item_materials_category_rv.view.*
 import kotlinx.android.synthetic.main.item_posts_rv.view.*
 
@@ -70,8 +80,13 @@ class ActivityMaterials : AppCompatActivity(),
 
     }
 
-    private val postAdapter = PostAdapter(ArrayList(0), itemListener)
-    private val postLayoutManager = LinearLayoutManager(this)
+
+    lateinit var posts: List<Post>
+    lateinit var postAdapter: PostAdapter
+    lateinit var scrollListener: EndlessRecyclerViewScrollListener
+    lateinit var postLayoutManager: RecyclerView.LayoutManager
+    var loadMorePosts: java.util.ArrayList<Post?> = java.util.ArrayList()
+
 
     private val categoryAdapter = CategoryAdapter(
         ArrayList(0),
@@ -119,11 +134,80 @@ class ActivityMaterials : AppCompatActivity(),
 
     }
 
-    override fun showCategories() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun setRVLayoutManager() {
+        postLayoutManager = LinearLayoutManager(this)
+        postsRV.layoutManager = postLayoutManager
+        postsRV.setHasFixedSize(true)
     }
 
-    override fun showPosts() {
+    override fun setRVScrollListener() {
+        postLayoutManager = LinearLayoutManager(this)
+        scrollListener =
+            EndlessRecyclerViewScrollListener(postLayoutManager as LinearLayoutManager)
+        scrollListener.setOnLoadMoreListener(object :
+            EndlessRecyclerViewScrollListener.OnLoadMoreListener {
+            override fun onLoadMore() {
+                Log.d(TAG, "스크롤리스너 onLoadMore() 실행! ")
+
+
+                //loadMorePosts 에 null값을 추가해서 로딩뷰를 만든다.
+                postAdapter.addLoadingView()
+                Log.d(TAG, "postAdapter.addLoadingView() 후 tempArrayList ? : ${postAdapter.tempArrayList}")
+                loadMorePosts.add(null) // 이렇게 넣어줘야 하나 ?.. 어댑터랑 연결해서 넣어줄 수 없나
+
+//                Log.d(TAG, "addLoadingView 실행 후 loadMorePosts 값 ? : ${loadMorePosts}")
+
+                //loadMorePosts 는 다음페이지 데이터를 받아올 때만 데이터를 추가하기 때문에 조건절로 비어있는지 확인해야함
+                if (!loadMorePosts.isEmpty()) {
+                    //loadMorePosts의 마지막 값이 null값이 있으면 무한스크롤 로딩 중이기 때문에 데이터를 받아오고, 로딩뷰를 제거한다.
+                    if (loadMorePosts[loadMorePosts.size - 1] == null) {
+                        Log.d(TAG, "loadMorePosts 마지막 아이템에 null값 있음 ")
+                        presenter.loadMorePosts(presenter.requestPosts())
+//                        showMorePosts()
+                    }
+                }
+
+            }
+        })
+        postsRV.addOnScrollListener(scrollListener)
+    }
+
+
+    override fun showPosts(posts: List<Post>) {
+        postAdapter = PostAdapter(itemListener)
+        postAdapter.addPosts(posts)
+        postAdapter.notifyDataSetChanged()
+        postsRV.adapter = postAdapter
+    }
+
+    override fun showMorePosts(posts: List<Post>) {
+
+        if(loadMorePosts[loadMorePosts.size-1] == null) {
+            //Use Handler if the items are loading too fast.
+            //If you remove it, the data will load so fast that you can't even see the LoadingView
+            Handler().postDelayed({
+                //Remove the Loading View
+                postAdapter.removeLoadingView()
+
+                //loadMorePosts에 있던 값을 다 지우고 추가로 받은 데이터 넣음
+                loadMorePosts.removeAll(loadMorePosts)
+                loadMorePosts.addAll(posts)
+
+                //We adding the data to our main ArrayList
+                postAdapter.addMorePosts(loadMorePosts)
+                //Change the boolean isLoading to false
+                scrollListener.setLoaded()
+                //Update the recyclerView in the main thread
+                postsRV.post {
+                    postAdapter.notifyDataSetChanged()
+                }
+            }, 1000)
+        }
+
+    }
+
+
+    override fun showCategories() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
