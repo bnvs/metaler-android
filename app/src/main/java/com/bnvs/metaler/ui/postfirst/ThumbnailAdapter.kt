88c17e670737fb70d2b1f08c1597fc0f0eb0e919
nukeolaf <yhjin04170@gmail.com>
2020-04-22 21:48:47 +0900

    [ADD] 게시글 작성 1단계 썸네일 보여주는 리사이클러뷰 어댑터추가

---
+++ app/src/main/java/com/bnvs/metaler/ui/postfirst/ThumbnailAdapter.kt
@@ -0,0 +1,41 @@
+package com.bnvs.metaler.ui.postfirst
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bnvs.metaler.R
+
+class ThumbnailAdapter : RecyclerView.Adapter<ThumbnailViewHolder>() {
+
+    private val images = mutableListOf<String>()
+
+    fun setImages(list: List<String>) {
+        this.images.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun addImage(imageUrl: String) {
+        this.images.add(imageUrl)
+        notifyDataSetChanged()
+    }
+
+    fun deleteImage(imageIndex: Int) {
+        this.images.removeAt(imageIndex)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThumbnailViewHolder {
+        val inflatedView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_thumbnail_rv, parent, false)
+        return ThumbnailViewHolder(inflatedView)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    override fun onBindViewHolder(holder: ThumbnailViewHolder, position: Int) {
+        holder.bind(images[position])
+    }
+}
