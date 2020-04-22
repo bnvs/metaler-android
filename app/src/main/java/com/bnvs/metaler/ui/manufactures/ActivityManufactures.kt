cb027d9e0e80954d3bb63480d4d20a8d3b9787be
ohseungyeon <sally37345>
2020-04-22 21:17:52 +0900

    [ADD] ActivityManufacutres.kt 북마크 버튼 눌렀을 때 추가, 삭제 되도록 기능 추가함. 필요없는 로그와 주석 지움

--- app/src/main/java/com/bnvs/metaler/ui/manufactures/ActivityManufactures.kt
+++ app/src/main/java/com/bnvs/metaler/ui/manufactures/ActivityManufactures.kt
@@ -51,36 +51,16 @@ class ActivityManufactures : AppCompatActivity(),
         ) {
 
             if (!isBookmark) {
-                Log.d(TAG, "isBookmark ? : ${isBookmark}")
-
-//            if(!posts[position]!!.is_bookmark) {
-
-                Log.d(TAG, "북마크 이미지 변경!  ")
-                Log.d(TAG, "clickedPostId ? : ${clickedPostId}")
-                Log.d(TAG, "position ? : ${position}")
-
-//                postAdapter.setBookmark(position)
-
-//                view.bookmarkBtn.setImageResource(R.drawable.ic_list_bookmark_active_x3)
-
-//                isBookmark == posts[position]!!.is_bookmark
-//                presenter.addBookmark(clickedPostId)
+                presenter.addBookmark(clickedPostId)
                 postAdapter.apply {
                     setBookmark(position)
                     notifyDataSetChanged()
-                    Log.d(TAG, "isBookmark ? : ${isBookmark}")
-
                 }
             } else {
-//                view.bookmarkBtn.setImageResource(R.drawable.ic_list_bookmark_inactive_x3)
-
-//                isBookmark == !posts[position]!!.is_bookmark
-//                presenter.deleteBookmark(clickedPostId)
+                presenter.deleteBookmark(clickedPostId)
                 postAdapter.apply {
                     setBookmark(position)
                     notifyDataSetChanged()
-                    Log.d(TAG, "isBookmark ? : ${isBookmark}")
-
                 }
             }
         }
