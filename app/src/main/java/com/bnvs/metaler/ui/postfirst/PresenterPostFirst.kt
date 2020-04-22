277aa1d05e3ed2dbb1edc5d28998e96ce7090122
nukeolaf <yhjin04170@gmail.com>
2020-04-22 19:07:32 +0900

    [ADD] 게시물 작성 1단계 작성 내용을 Presenter 가 addEditPostRequest 전역변수로 갖고 있도록 함

--- app/src/main/java/com/bnvs/metaler/ui/postfirst/PresenterPostFirst.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postfirst/PresenterPostFirst.kt
@@ -1,5 +1,6 @@
 package com.bnvs.metaler.ui.postfirst
 
+import com.bnvs.metaler.data.addeditpost.model.AddEditPostRequest
 import com.bnvs.metaler.data.addeditpost.source.repository.AddEditPostRepository
 import com.bnvs.metaler.data.postdetails.source.repository.PostDetailsRepository
 import com.bnvs.metaler.network.NetworkUtil
@@ -12,6 +13,17 @@ class PresenterPostFirst(
 
     private val addEditPostRepository = AddEditPostRepository()
     private val postDetailsRepository = PostDetailsRepository()
+
+    private var addEditPostRequest = AddEditPostRequest(
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    )
+
 
     override fun start() {
         if (postId != 0) {
@@ -32,6 +44,47 @@ class PresenterPostFirst(
                 view.showPostDetailLoadFailedDialog(NetworkUtil.getErrorMessage(e))
             }
         )
+    }
+
+    override fun setCategory(categoryId: Int) {
+        addEditPostRequest.category_id = categoryId
+        if (categoryType == "MATERIALS") {
+            val category = when (categoryId) {
+                2 -> "적동/황동"
+                3 -> "스테인리스"
+                4 -> "알루미늄"
+                5 -> "철"
+                6 -> "공구"
+                7 -> "화학 약품류"
+                8 -> "니켈/티타늄"
+                9 -> "기타"
+                else -> return
+            }
+            view.setCategory(category)
+        }
+    }
+
+    override fun setTitle(title: String) {
+        addEditPostRequest.title = title
+        view.setTitle(title)
+    }
+
+    override fun setPrice(price: Int) {
+        addEditPostRequest.price = price
+        view.setPrice(price)
+    }
+
+    override fun setPriceType(priceType: String) {
+        addEditPostRequest.price_type = priceType
+        view.setPriceType(priceType)
+    }
+
+    override fun setImage() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setContents() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getImageFromAlbum() {
