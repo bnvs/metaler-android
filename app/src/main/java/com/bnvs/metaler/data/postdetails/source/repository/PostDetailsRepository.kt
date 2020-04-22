b781659b3ea26ab4ba1252dca9a94f6e557fecea
nukeolaf <yhjin04170@gmail.com>
2020-04-22 17:09:34 +0900

    [MD] postDetailsRepository 이름 오타 수정

--- app/src/main/java/com/bnvs/metaler/data/postdetails/source/repository/PostDetailsRepository.kt
+++ app/src/main/java/com/bnvs/metaler/data/postdetails/source/repository/PostDetailsRepository.kt
@@ -1,20 +1,19 @@
 package com.bnvs.metaler.data.postdetails.source.repository
 
-import android.content.Context
 import com.bnvs.metaler.data.postdetails.model.PostDetails
 import com.bnvs.metaler.data.postdetails.source.PostDetailsDataSource
 import com.bnvs.metaler.data.postdetails.source.remote.PostDetailsRemoteDataSource
 
-class PostDetailsRepository(context: Context) :
+class PostDetailsRepository :
     PostDetailsDataSource {
 
-    private val postDetialsRemoteDataSource = PostDetailsRemoteDataSource
+    private val postDetailsRemoteDataSource = PostDetailsRemoteDataSource
 
     override fun getPostDetails(
         postId: Int,
         onSuccess: (response: PostDetails) -> Unit,
         onFailure: (e: Throwable) -> Unit
     ) {
-        postDetialsRemoteDataSource.getPostDetails(postId, onSuccess, onFailure)
+        postDetailsRemoteDataSource.getPostDetails(postId, onSuccess, onFailure)
     }
 }
