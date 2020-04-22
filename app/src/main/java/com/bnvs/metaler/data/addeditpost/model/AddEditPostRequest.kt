6f520c6b5ceb0dfd8e3262b38e07c0cf8fa2dfc9
nukeolaf <yhjin04170@gmail.com>
2020-04-22 18:40:39 +0900

    [RF] AddEditPostRequest 각각의 값에 null 값이 들어갈 수 있도록 수정

--- app/src/main/java/com/bnvs/metaler/data/addeditpost/model/AddEditPostRequest.kt
+++ app/src/main/java/com/bnvs/metaler/data/addeditpost/model/AddEditPostRequest.kt
@@ -1,11 +1,11 @@
 package com.bnvs.metaler.data.addeditpost.model
 
 data class AddEditPostRequest(
-    val category_id: Int,
-    val title: String,
-    val content: String,
-    val price: Int,
-    val price_type: String,
-    val attach_ids: List<Int>,
-    val tags: List<AddEditPostTag>
+    val category_id: Int?,
+    val title: String?,
+    val content: String?,
+    val price: Int?,
+    val price_type: String?,
+    val attach_ids: List<Int>?,
+    val tags: List<AddEditPostTag>?
 )
