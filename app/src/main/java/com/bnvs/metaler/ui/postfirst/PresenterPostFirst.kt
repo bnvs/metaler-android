e427e4f9640579014d4f8cc7bde9c0ffed96b354
nukeolaf <yhjin04170@gmail.com>
2020-04-27 02:36:21 +0900

    [FIX] overload 에러 수정

--- app/src/main/java/com/bnvs/metaler/ui/postfirst/PresenterPostFirst.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postfirst/PresenterPostFirst.kt
@@ -65,17 +65,6 @@ class PresenterPostFirst(
         )
     }
 
-    override fun getCategories() {
-        categoriesRepository.getCategories(
-            onSuccess = { response ->
-                categories = response
-            },
-            onFailure = { e ->
-                view.showGetCategoriesFailedToast(NetworkUtil.getErrorMessage(e))
-            }
-        )
-    }
-
     override fun populatePost(postId: Int) {
         postDetailsRepository.getPostDetails(
             postId,
@@ -337,10 +326,6 @@ class PresenterPostFirst(
             return
         }
 
-        view.showPostSecondUi(addEditPostRequest)
-    }
-
-    override fun openPostSecond() {
         view.showPostSecondUi(addEditPostRequest)
     }
 }
