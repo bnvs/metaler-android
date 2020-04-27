a912916deaceb00a77214abef4d1d51a5626b793
nukeolaf <yhjin04170@gmail.com>
2020-04-28 06:26:07 +0900

    [ADD] ContractPostSecond (게시물 수정시) 기존 태그 내용 view 에 set 하는 함수 작성

--- app/src/main/java/com/bnvs/metaler/ui/postsecond/ContractPostSecond.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postsecond/ContractPostSecond.kt
@@ -10,10 +10,16 @@ interface ContractPostSecond {
         fun setShopNameTagInputAdapter()
         fun setWorkTagInputAdapter()
         fun setTagInputAdapter()
+        fun setShopNameInput(tags: String)
+        fun setWorkInput(tags: String)
+        fun setTagInput(tags: String)
     }
 
     interface Presenter : BasePresenter {
         fun getAddEditPostRequest(intent: Intent)
         fun populatePost()
+        fun setShopNameInput()
+        fun setWorkInput()
+        fun setTagInput()
     }
 }
