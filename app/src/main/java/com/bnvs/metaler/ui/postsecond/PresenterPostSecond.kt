22d5be41e6fffc1c200f06d5e82f5728d0da94fa
nukeolaf <yhjin04170@gmail.com>
2020-04-28 06:27:51 +0900

    [ADD] PresenterPostSecond (게시물 수정시) 기존 태그 내용 view 에 set 하는 함수 작성

--- app/src/main/java/com/bnvs/metaler/ui/postsecond/PresenterPostSecond.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postsecond/PresenterPostSecond.kt
@@ -2,6 +2,7 @@ package com.bnvs.metaler.ui.postsecond
 
 import android.content.Intent
 import com.bnvs.metaler.data.addeditpost.model.AddEditPostRequest
+import com.bnvs.metaler.data.addeditpost.model.PostTag
 
 class PresenterPostSecond(
     private val categoryType: String,
@@ -10,11 +11,15 @@ class PresenterPostSecond(
 ) : ContractPostSecond.Presenter {
 
     private lateinit var addEditPostRequest: AddEditPostRequest
+    private var shopNameTagString = ""
+    private var workTagString = ""
+    private var tagString = ""
 
     override fun start() {
         distinguishMaterialOrManufacture()
         if (postId != null) {
-
+            getTagString(addEditPostRequest.tags)
+            populatePost()
         }
     }
 
@@ -30,10 +35,38 @@ class PresenterPostSecond(
     }
 
     override fun populatePost() {
-
+        when (categoryType) {
+            "MATERIALS" -> {
+                setShopNameInput()
+                setTagInput()
+            }
+            "MANUFACTURES" -> {
+                setShopNameInput()
+                setWorkInput()
+                setTagInput()
+            }
+        }
     }
 
-    override fun getAddEditPostRequest(intent: Intent) {
-        addEditPostRequest = intent.getSerializableExtra("addEditPostRequest") as AddEditPostRequest
+    override fun setShopNameInput() {
+        view.setShopNameInput(shopNameTagString)
+    }
+
+    override fun setWorkInput() {
+        view.setWorkInput(workTagString)
+    }
+
+    override fun setTagInput() {
+        view.setTagInput(tagString)
+    }
+
+    private fun getTagString(tags: List<PostTag>) {
+        for (tag in tags) {
+            when (tag.type) {
+                "store" -> shopNameTagString += "#${tag.name} "
+                "work" -> workTagString += "#${tag.name} "
+                "etc" -> tagString += "#${tag.name} "
+            }
+        }
     }
 }
