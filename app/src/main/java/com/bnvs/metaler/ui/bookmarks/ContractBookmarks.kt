8c10a79800032cb2a5ae55333e483b864234c94f
ohseungyeon <sally37345>
2020-04-28 01:14:49 +0900

    [ADD] ContractBookmarks.kt loadMaterialsPost,loadManufacturePost 를 loadBookmarkPosts 이 함수로 합침

--- app/src/main/java/com/bnvs/metaler/ui/bookmarks/ContractBookmarks.kt
+++ app/src/main/java/com/bnvs/metaler/ui/bookmarks/ContractBookmarks.kt
@@ -2,26 +2,24 @@ package com.bnvs.metaler.ui.bookmarks
 
 import com.bnvs.metaler.BasePresenter
 import com.bnvs.metaler.BaseView
+import com.bnvs.metaler.data.bookmarks.model.Bookmark
 import com.bnvs.metaler.data.bookmarks.model.BookmarksRequest
-import com.bnvs.metaler.data.posts.model.Post
 import com.bnvs.metaler.util.TapBarContract
 
 interface ContractBookmarks {
     interface View : BaseView<Presenter>, TapBarContract.View {
         fun showPostDetailUi(postId: Int)
 
-        fun showMaterialsList(posts: List<Post>)
+        fun showMaterialsList(posts: List<Bookmark>)
 
-        fun showManufacturesList(posts: List<Post>)
+        fun showManufacturesList(posts: List<Bookmark>)
 
         fun showBookmarkDeleteDialog(postId: Int)
 
     }
 
     interface Presenter : BasePresenter, TapBarContract.Presenter {
-        fun loadMaterialsPost(bookmarksRequest: BookmarksRequest)
-
-        fun loadManufacturePost(bookmarksRequest: BookmarksRequest)
+        fun loadBookmarkPosts(bookmarksRequest: BookmarksRequest)
 
         fun requestPosts(categoryId: Int): BookmarksRequest
 
