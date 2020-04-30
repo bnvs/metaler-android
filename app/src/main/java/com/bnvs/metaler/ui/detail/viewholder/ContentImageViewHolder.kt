e209fcf96decef7fc19bbc3b1d9c7223e9f0856b
nukeolaf <yhjin04170@gmail.com>
2020-05-01 05:05:57 +0900

    [ADD] Glide 이미지 로딩시 보여주는 이미지 추가

--- app/src/main/java/com/bnvs/metaler/ui/detail/viewholder/ContentImageViewHolder.kt
+++ app/src/main/java/com/bnvs/metaler/ui/detail/viewholder/ContentImageViewHolder.kt
@@ -1,9 +1,15 @@
 package com.bnvs.metaler.ui.detail.viewholder
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bnvs.metaler.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.item_content_img_rv.view.*
 
 class ContentImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -11,7 +17,30 @@ class ContentImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     fun bind(imageUrl: String) {
         Glide.with(itemView)
             .load(imageUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    itemView.contentImgLoading.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    itemView.contentImgLoading.visibility = View.GONE
+                    return false
+                }
+            })
             .transform(CenterCrop())
+            .error(R.drawable.ic_broken_image_black_24dp)
             .into(itemView.contentImg)
     }
 
--- app/src/main/java/com/bnvs/metaler/ui/postfirst/ThumbnailViewHolder.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postfirst/ThumbnailViewHolder.kt
@@ -1,8 +1,14 @@
 package com.bnvs.metaler.ui.postfirst
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bnvs.metaler.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.item_thumbnail_rv.view.*
 
 class ThumbnailViewHolder(
@@ -21,6 +27,29 @@ class ThumbnailViewHolder(
     fun bind(attachUrl: String) {
         Glide.with(itemView)
             .load(attachUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    itemView.thumbnailLoading.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    itemView.thumbnailLoading.visibility = View.GONE
+                    return false
+                }
+            })
+            .error(R.drawable.ic_broken_image_black_24dp)
             .into(itemView.thumbnailImg)
     }
 
