ad72433ebc5cc945b45c9904b58e95ad072b26fa
ohseungyeon <sally37345>
2020-04-30 23:47:09 +0900

    [ADD] TapBarContract.kt 각 탭으로 이동 시, FLAG_ACTIVITY_CLEAR_TOP 플래그 사용

--- app/src/main/java/com/bnvs/metaler/util/TapBarContract.kt
+++ app/src/main/java/com/bnvs/metaler/util/TapBarContract.kt
@@ -18,46 +18,56 @@ interface TapBarContract {
 
     interface View {
         fun showHomeUi(context: Context) {
-            Intent(context, ActivityHome::class.java).also {
-                context.startActivity(it)
-            }
+            Intent(context, ActivityHome::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }.also { context.startActivity(it) }
         }
 
         fun showMaterialsUi(context: Context) {
-            Intent(context, ActivityMaterials::class.java).also {
-                context.startActivity(it)
-            }
+            Intent(context, ActivityMaterials::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }.also { context.startActivity(it) }
         }
 
         fun showManufacturesUi(context: Context) {
-            Intent(context, ActivityManufactures::class.java).also {
-                context.startActivity(it)
-            }
+            Intent(context, ActivityManufactures::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }.also { context.startActivity(it) }
         }
 
         fun showBookmarksUi(context: Context) {
-            Intent(context, ActivityBookmarks::class.java).also {
-                context.startActivity(it)
-            }
+            Intent(context, ActivityBookmarks::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }.also { context.startActivity(it) }
         }
 
         fun showMyPageUi(context: Context) {
-            Intent(context, ActivityMyPage::class.java).also {
-                context.startActivity(it)
-            }
+            Intent(context, ActivityMyPage::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }.also { context.startActivity(it) }
         }
     }
 
     interface Presenter {
-        fun openHome(view: View, context: Context) { view.showHomeUi(context) }
+        fun openHome(view: View, context: Context) {
+            view.showHomeUi(context)
+        }
 
-        fun openMaterials(view: View, context: Context) { view.showMaterialsUi(context) }
+        fun openMaterials(view: View, context: Context) {
+            view.showMaterialsUi(context)
+        }
 
-        fun openManufactures(view: View, context: Context) { view.showManufacturesUi(context) }
+        fun openManufactures(view: View, context: Context) {
+            view.showManufacturesUi(context)
+        }
 
-        fun openBookmarks(view: View, context: Context) { view.showBookmarksUi(context) }
+        fun openBookmarks(view: View, context: Context) {
+            view.showBookmarksUi(context)
+        }
 
-        fun openMyPage(view: View, context: Context) { view.showMyPageUi(context) }
+        fun openMyPage(view: View, context: Context) {
+            view.showMyPageUi(context)
+        }
     }
 
 }
