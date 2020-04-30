36477ce112cd1a91a893918f097736cd7bf4055c
nukeolaf <yhjin04170@gmail.com>
2020-05-01 05:28:39 +0900

    [MD] Glide 이미지 로딩시 보여주는 썸네일은 프로그래스바 돌아가도록 수정

--- app/src/main/java/com/bnvs/metaler/ui/postfirst/ThumbnailViewHolder.kt
+++ app/src/main/java/com/bnvs/metaler/ui/postfirst/ThumbnailViewHolder.kt
@@ -1,6 +1,11 @@
 package com.bnvs.metaler.ui.postfirst
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bnvs.metaler.R
@@ -21,6 +26,15 @@ class ThumbnailViewHolder(
         itemView.setOnLongClickListener {
             itemClick(adapterPosition)
             true
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            itemView.thumbnailLoading.indeterminateDrawable.colorFilter =
+                BlendModeColorFilter(R.color.colorLightPurple, BlendMode.SRC_ATOP)
+        } else {
+            itemView.thumbnailLoading.indeterminateDrawable.setColorFilter(
+                Color.GRAY,
+                PorterDuff.Mode.MULTIPLY
+            )
         }
     }
 
