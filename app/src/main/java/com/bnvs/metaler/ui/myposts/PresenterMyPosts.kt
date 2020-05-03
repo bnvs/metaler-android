13f35ec4e99b0d0d45e31a2a66d57ae56e6d8724
ohseungyeon <sally37345>
2020-05-04 03:27:34 +0900

    [ADD] PresneterMyPosts.kt 게시글 데이터 가져올 떄 필요한 기능 추가함

--- app/src/main/java/com/bnvs/metaler/ui/myposts/PresenterMyPosts.kt
+++ app/src/main/java/com/bnvs/metaler/ui/myposts/PresenterMyPosts.kt
@@ -1,13 +1,70 @@
 package com.bnvs.metaler.ui.myposts
 
 import android.content.Context
+import android.widget.Toast
+import com.bnvs.metaler.data.myposts.model.MyPosts
+import com.bnvs.metaler.data.myposts.model.MyPostsRequest
+import com.bnvs.metaler.data.myposts.source.repository.MyPostsRepository
+import com.bnvs.metaler.network.NetworkUtil
 
 class PresenterMyPosts(
     private val context: Context,
     private val view: ContractMyPosts.View
 ) : ContractMyPosts.Presenter {
-    override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    private val myPostsRepository: MyPostsRepository = MyPostsRepository()
+    private lateinit var myPostsRequest: MyPostsRequest
+
+    private var pageNum: Int = 0
+    private var categoryType: String = ""
+
+    init {
+        view.presenter = this
     }
 
+    override fun start() {
+        loadMyPosts(requestPosts("manufacture"))
+    }
+
+    override fun requestPosts(categoryType: String): MyPostsRequest {
+        pageNum++
+        this.categoryType = categoryType
+        myPostsRequest = MyPostsRequest(
+            pageNum,
+            10,
+            categoryType
+        )
+        return myPostsRequest
+    }
+
+    override fun loadMyPosts(myPostsRequest: MyPostsRequest) {
+        myPostsRepository.getMyPosts(
+            myPostsRequest,
+            onSuccess = {response: MyPosts ->
+                view.showMyPostsList(response.posts)
+            },
+            onFailure =  {
+                    e ->
+                Toast.makeText(
+                    context,
+                    "서버 통신 실패 : ${NetworkUtil.getErrorMessage(e)}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        )
+    }
+
+    override fun openMaterialsList() {
+        pageNum = 0
+        loadMyPosts(requestPosts("manufacture"))
+    }
+
+    override fun openManufacturesList() {
+        pageNum = 0
+        loadMyPosts(requestPosts("materials"))
+    }
+
+    override fun openPostDetail(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
