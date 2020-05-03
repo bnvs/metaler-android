78294919c1ba57eedd7363feec32e3b533900b9c
ohseungyeon <sally37345>
2020-05-04 03:36:00 +0900

    [ADD] ActivityMyPosts.kt 아이템 리스너 추가함

--- app/src/main/java/com/bnvs/metaler/ui/myposts/ActivityMyPosts.kt
+++ app/src/main/java/com/bnvs/metaler/ui/myposts/ActivityMyPosts.kt
@@ -1,6 +1,7 @@
 package com.bnvs.metaler.ui.myposts
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bnvs.metaler.R
@@ -32,6 +33,21 @@ class ActivityMyPosts : AppCompatActivity(), ContractMyPosts.View {
         }
     }
 
+    /**
+     * 내가 쓴 글 확인 재료/가공 카테고리 눌렀을 때 보여지는 재료/가공 게시물 리사이클러뷰 아이템에 달아줄 클릭리스너입니다
+     * 아이템 클릭 시, 클릭한 게시물의 post_id 를 presenter 에 전달합니다.
+     * onPostClick -> 게시물을 클릭한 경우
+     * onMoreButtonClick -> 더보기 버튼을 클릭한 경우. 수정, 삭제 메뉴 다이얼로그를 실행합니다.
+     * */
+    private var myPostsItemListener: MyPostsItemListener = object : MyPostsItemListener {
+        override fun onPostClick(clickedPostId: Int) {
+            presenter.openPostDetail(clickedPostId)
+        }
+
+        override fun onMoreButtonClick(view: View, clickedPostId: Int, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
 
     //리사이클러뷰
     private fun setRVLayoutManager() {
