86eebedef5dde069fc248499ffa79cbb4042067e
ohseungyeon <sally37345>
2020-05-03 19:18:57 +0900

    [ADD] ContractMyPage.kt 파일 추가

---
+++ app/src/main/java/com/bnvs/metaler/ui/myposts/ContractMyPage.kt
@@ -0,0 +1,13 @@
+package com.bnvs.metaler.ui.myposts
+
+import com.bnvs.metaler.BasePresenter
+import com.bnvs.metaler.BaseView
+import com.bnvs.metaler.util.TapBarContract
+
+interface ContractMyPage {
+    interface View : BaseView<Presenter>, TapBarContract.View {
+    }
+
+    interface Presenter : BasePresenter, TapBarContract.Presenter {
+    }
+}
