ccc429d4a0b6df804c43a0e0f60219ad9d3554c4
ohseungyeon <sally37345>
2020-05-04 03:35:30 +0900

    [ADD] ActivityMyPosts.kt 리사이클러뷰에 사용하는 변수 추가, onCreate에 프레젠터 생성 및 start()실행

--- app/src/main/java/com/bnvs/metaler/ui/myposts/ActivityMyPosts.kt
+++ app/src/main/java/com/bnvs/metaler/ui/myposts/ActivityMyPosts.kt
@@ -2,12 +2,48 @@ package com.bnvs.metaler.ui.myposts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.bnvs.metaler.R
+import com.bnvs.metaler.data.myposts.model.MyPost
+import com.bnvs.metaler.util.EndlessRecyclerViewScrollListener
 
-class ActivityMyPosts : AppCompatActivity() {
+class ActivityMyPosts : AppCompatActivity(), ContractMyPosts.View {
+
+    private val TAG = "ActivityMyPosts"
+
+    override lateinit var presenter: ContractMyPosts.Presenter
+
+    lateinit var myPostAdapter: MyPostsAdapter
+    lateinit var scrollListener: EndlessRecyclerViewScrollListener
+    lateinit var myPostLayoutManager: RecyclerView.LayoutManager
+    var loadMoreMyPosts: MutableList<MyPost?> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_posts)
+
+        presenter = PresenterMyPosts(
+            this@ActivityMyPosts,
+            this@ActivityMyPosts
+        )
+
+        presenter.run {
+            start()
+        }
     }
+
+
+    //리사이클러뷰
+    private fun setRVLayoutManager() {
+
+    }
+
+    override fun showMyPostsList(myPosts: List<MyPost>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showPostDetailUi(postId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
