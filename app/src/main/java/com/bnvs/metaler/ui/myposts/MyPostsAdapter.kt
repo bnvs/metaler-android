75e78ef0077ea59d286796ebaf11fa5c8f02f22f
ohseungyeon <sally37345>
2020-05-04 01:07:33 +0900

    [ADD] MyPosstsAdapter.kt 어댑터 클래스의 함수 추가함

--- app/src/main/java/com/bnvs/metaler/ui/myposts/MyPostsAdapter.kt
+++ app/src/main/java/com/bnvs/metaler/ui/myposts/MyPostsAdapter.kt
@@ -1,2 +1,21 @@
 package com.bnvs.metaler.ui.myposts
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyPostsAdapter(
+    private var myPostsItemListener: MyPostsItemListener
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
