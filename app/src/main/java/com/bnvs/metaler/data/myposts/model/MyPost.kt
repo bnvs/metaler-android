fb2f5ce3e5db2bc60630790c103c29058463a85c
ohseungyeon <sally37345>
2020-05-04 15:46:52 +0900

    [ADD] MyPost.kt 데이터클래스에 thumbnail, tags 추가함

--- app/src/main/java/com/bnvs/metaler/data/myposts/model/MyPost.kt
+++ app/src/main/java/com/bnvs/metaler/data/myposts/model/MyPost.kt
@@ -1,5 +1,7 @@
 package com.bnvs.metaler.data.myposts.model
 
+import com.bnvs.metaler.data.addeditpost.model.PostTag
+
 data class MyPost(
     val id: Int,
     val title: String,
@@ -9,5 +11,7 @@ data class MyPost(
     val profile_nickname: String,
     val liked: Int,
     val disliked: Int,
+    val thumbnail: String,
+    val tags: List<PostTag>,
     val is_bookmark: Boolean
 )
