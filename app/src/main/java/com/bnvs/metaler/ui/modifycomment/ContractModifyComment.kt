987f22ecea1629a79816508bc405cb5526dbe5cf
nukeolaf <yhjin04170@gmail.com>
2020-05-05 17:46:25 +0900

    [ADD] 댓글 수정 Ui MVP 클래스 추가

---
+++ app/src/main/java/com/bnvs/metaler/ui/modifycomment/ContractModifyComment.kt
@@ -0,0 +1,11 @@
+package com.bnvs.metaler.ui.modifycomment
+
+import com.bnvs.metaler.BasePresenter
+import com.bnvs.metaler.BaseView
+
+interface ContractModifyComment {
+    interface View : BaseView<Presenter>
+
+    interface Presenter : BasePresenter
+
+}
---
+++ app/src/main/java/com/bnvs/metaler/ui/modifycomment/PresenterModifyComment.kt
@@ -0,0 +1,8 @@
+package com.bnvs.metaler.ui.modifycomment
+
+class PresenterModifyComment : ContractModifyComment.Presenter {
+
+    override fun start() {
+
+    }
+}
