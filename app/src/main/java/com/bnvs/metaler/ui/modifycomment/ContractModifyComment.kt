6933388d269c4c35aeda733caeab76068a2bb777
nukeolaf <yhjin04170@gmail.com>
2020-05-05 22:18:07 +0900

    [ADD] 댓글 수정 액티비티 기능 추가

--- app/src/main/java/com/bnvs/metaler/ui/modifycomment/ContractModifyComment.kt
+++ app/src/main/java/com/bnvs/metaler/ui/modifycomment/ContractModifyComment.kt
@@ -2,10 +2,22 @@ package com.bnvs.metaler.ui.modifycomment
 
 import com.bnvs.metaler.BasePresenter
 import com.bnvs.metaler.BaseView
+import com.bnvs.metaler.data.comments.model.Comment
 
 interface ContractModifyComment {
-    interface View : BaseView<Presenter>
+    interface View : BaseView<Presenter> {
+        fun showComment(comment: Comment)
+        fun showModifiedComment(comment: String)
+        fun showModifyCommentSuccessToast()
+        fun setCommentToModify(comment: String)
+        fun hideSoftInput()
+        fun showSoftInput()
+        fun showErrorToast(errorMessage: String)
+    }
 
-    interface Presenter : BasePresenter
+    interface Presenter : BasePresenter {
+        val content: String
+        fun modifyComment(comment: String)
+    }
 
 }
