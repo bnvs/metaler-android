0b13b9246b94b5692fadae93a9e59d7afb04860f
nukeolaf <yhjin04170@gmail.com>
2020-05-05 18:02:53 +0900

    [MD] 게시물 목록 조회 관련 Model 세분화
    search_type 에 따라 3가지로 나눔

--- app/src/main/java/com/bnvs/metaler/data/posts/model/PostsRequest.kt
+++ app/src/main/java/com/bnvs/metaler/data/posts/model/PostsRequest.kt
@@ -3,7 +3,5 @@ package com.bnvs.metaler.data.posts.model
 data class PostsRequest(
     val category_id: Int,
     val page: Int,
-    val limit: Int,
-    val search_type: String?,
-    val search_word: String?
+    val limit: Int
 )
