0d626085559145622b752c9dd296003dd2ee533a
nukeolaf <yhjin04170@gmail.com>
2020-05-07 04:34:08 +0900

    [ADD] 이용약관 확인 액티비티에서 webView 에 header 추가하여 url 보여주는 기능 추가

--- app/src/main/java/com/bnvs/metaler/ui/termscheck/ActivityTermsCheck.kt
+++ app/src/main/java/com/bnvs/metaler/ui/termscheck/ActivityTermsCheck.kt
@@ -3,12 +3,27 @@ package com.bnvs.metaler.ui.termscheck
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bnvs.metaler.R
+import com.bnvs.metaler.data.token.source.repository.TokenRepository
+import kotlinx.android.synthetic.main.activity_terms_check.*
 
 class ActivityTermsCheck : AppCompatActivity() {
+
+    private lateinit var tokenRepository: TokenRepository
+    private val url = "http://metaler.kr/users/terms"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_check)
 
+        tokenRepository = TokenRepository(this)
+        tokenRepository.getAccessToken(
+            onTokenLoaded = { token ->
+                val header = mapOf("Authorization" to token.access_token)
+                termsCheckWebView.loadUrl(url, header)
+            },
+            onTokenNotExist = {
+                finish()
+            }
+        )
     }
 }
