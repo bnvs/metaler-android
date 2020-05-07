244982414d741c03e657875a05de23aed00ab6b1
nukeolaf <yhjin04170@gmail.com>
2020-05-07 20:00:02 +0900

    [ADD] 마이페이지 약관 확인 기능 완료

--- app/src/main/java/com/bnvs/metaler/ui/termscheck/ActivityTermsCheck.kt
+++ app/src/main/java/com/bnvs/metaler/ui/termscheck/ActivityTermsCheck.kt
@@ -1,29 +1,71 @@
 package com.bnvs.metaler.ui.termscheck
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bnvs.metaler.R
-import com.bnvs.metaler.data.token.source.repository.TokenRepository
+import com.bnvs.metaler.data.user.modification.model.Terms
+import com.bnvs.metaler.data.user.modification.source.UserModificationRepository
+import com.bnvs.metaler.network.NetworkUtil
 import kotlinx.android.synthetic.main.activity_terms_check.*
 
 class ActivityTermsCheck : AppCompatActivity() {
 
-    private lateinit var tokenRepository: TokenRepository
-    private val url = "http://metaler.kr/users/terms"
+    private lateinit var userModificationRepository: UserModificationRepository
+    private lateinit var terms: Terms
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_check)
 
-        tokenRepository = TokenRepository(this)
-        tokenRepository.getAccessToken(
-            onTokenLoaded = { token ->
-                val header = mapOf("Authorization" to token.access_token)
-                termsCheckWebView.loadUrl(url, header)
+        userModificationRepository = UserModificationRepository()
+        getTermLinks()
+    }
+
+    private fun getTermLinks() {
+        userModificationRepository.getTerms(
+            onSuccess = { response ->
+                terms = response
+                setClickListeners()
             },
-            onTokenNotExist = {
+            onFailure = { e ->
+                makeToast(NetworkUtil.getErrorMessage(e))
                 finish()
             }
         )
+    }
+
+    private fun setClickListeners() {
+        backBtn.setOnClickListener { finish() }
+        firstTermMoreBtn.setOnClickListener { showTermsWebView(terms.service) }
+        secondTermMoreBtn.setOnClickListener { showTermsWebView(terms.privacy) }
+        thirdTermMoreBtn.setOnClickListener { showTermsWebView(terms.additional) }
+        fourthTermMoreBtn.setOnClickListener { showTermsWebView(terms.advertising) }
+    }
+
+    private fun showTermsWebView(url: String) {
+        val webView = WebView(this).apply {
+            loadUrl(url)
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    view!!.loadUrl(url)
+                    return true
+                }
+            }
+        }
+        AlertDialog.Builder(this@ActivityTermsCheck)
+            .setTitle("Metaler")
+            .setView(webView)
+            .setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun makeToast(message: String) {
+        Toast.makeText(this@ActivityTermsCheck, message, Toast.LENGTH_LONG).show()
     }
 }
