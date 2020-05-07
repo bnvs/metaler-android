b9fd25f26e6c53084a558e72e2ae4e65902a9a82
nukeolaf <yhjin04170@gmail.com>
2020-05-07 20:20:43 +0900

    [ADD] 회원가입 약관 동의 페이지에서 약관 더보기 버튼 기능 추가

--- app/src/main/java/com/bnvs/metaler/ui/termsagree/ActivityTermsAgree.kt
+++ app/src/main/java/com/bnvs/metaler/ui/termsagree/ActivityTermsAgree.kt
@@ -2,12 +2,16 @@ package com.bnvs.metaler.ui.termsagree
 
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bnvs.metaler.R
 import com.bnvs.metaler.data.user.certification.model.AddUserRequest
 import com.bnvs.metaler.ui.jobinput.ActivityJobInput
+import com.bnvs.metaler.ui.login.ActivityLogin
 import kotlinx.android.synthetic.main.activity_terms_agree.*
 import org.json.JSONObject
 
@@ -31,12 +35,28 @@ class ActivityTermsAgree : AppCompatActivity(),
         initClickListeners()
 
         presenter.apply {
+            start()
             getAddUserRequest(intent)
         }
     }
 
-    override fun showTermsWebView() {
-        // TODO : 약관 내용 보여주는 코드 추가
+    override fun showTermsWebView(url: String) {
+        val webView = WebView(this).apply {
+            loadUrl(url)
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    view!!.loadUrl(url)
+                    return true
+                }
+            }
+        }
+        AlertDialog.Builder(this@ActivityTermsAgree)
+            .setTitle("Metaler")
+            .setView(webView)
+            .setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
     override fun showEssentialAgreeNotCheckedDialog() {
@@ -44,6 +64,17 @@ class ActivityTermsAgree : AppCompatActivity(),
             setTitle(getString(R.string.terms_alert))
                 .setMessage(getString(R.string.essential_terms_guide))
                 .show()
+        }
+    }
+
+    override fun makeToast(message: String) {
+        Toast.makeText(this@ActivityTermsAgree, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun restartApp() {
+        finishAffinity()
+        Intent(this, ActivityLogin::class.java).also {
+            startActivity(it)
         }
     }
 
@@ -56,6 +87,7 @@ class ActivityTermsAgree : AppCompatActivity(),
 
     private fun initClickListeners() {
         setCheckBoxes()
+        setMoreButtons()
         setNextButton()
     }
 
@@ -71,15 +103,22 @@ class ActivityTermsAgree : AppCompatActivity(),
         fourthCheckBtn.setOnClickListener { onCheckChanged(fourthCheckBtn) }
     }
 
+    private fun setMoreButtons() {
+        firstAgreeMoreBtn.setOnClickListener { presenter.openTerms(1) }
+        secondAgreeMoreBtn.setOnClickListener { presenter.openTerms(2) }
+        thirdAgreeMoreBtn.setOnClickListener { presenter.openTerms(3) }
+        fourthAgreeMoreBtn.setOnClickListener { presenter.openTerms(4) }
+    }
+
     private fun onCheckChanged(compoundButton: CompoundButton) {
-        when(compoundButton.id) {
+        when (compoundButton.id) {
             R.id.allCheckBtn -> {
                 if (allCheckBtn.isChecked) {
                     firstCheckBtn.isChecked = true
                     secondCheckBtn.isChecked = true
                     thirdCheckBtn.isChecked = true
                     fourthCheckBtn.isChecked = true
-                }else {
+                } else {
                     firstCheckBtn.isChecked = false
                     secondCheckBtn.isChecked = false
                     thirdCheckBtn.isChecked = false
@@ -89,16 +128,16 @@ class ActivityTermsAgree : AppCompatActivity(),
             else -> {
                 allCheckBtn.isChecked = (
                         firstCheckBtn.isChecked
-                        && secondCheckBtn.isChecked
-                        && thirdCheckBtn.isChecked
-                        && fourthCheckBtn.isChecked)
+                                && secondCheckBtn.isChecked
+                                && thirdCheckBtn.isChecked
+                                && fourthCheckBtn.isChecked)
             }
         }
     }
 
     private fun checkAgrees(): JSONObject {
         val result = JSONObject()
-        return if(firstCheckBtn.isChecked && secondCheckBtn.isChecked) {
+        return if (firstCheckBtn.isChecked && secondCheckBtn.isChecked) {
             result.run {
                 put("valid", true)
                 put("firstTerm", firstCheckBtn.isChecked)
