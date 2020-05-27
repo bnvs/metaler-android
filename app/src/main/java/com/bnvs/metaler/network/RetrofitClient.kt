0073168f21eef60962444857b819991398f2beee
nukeolaf <yhjin04170@gmail.com>
2020-05-28 00:20:09 +0900

    [RF] RetrofitClient 의 HeaderInterceptor 클래스파일로 모듈화

--- app/src/main/java/com/bnvs/metaler/network/RetrofitClient.kt
+++ app/src/main/java/com/bnvs/metaler/network/RetrofitClient.kt
@@ -1,7 +1,6 @@
 package com.bnvs.metaler.network
 
 import com.bnvs.metaler.BuildConfig
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -11,35 +10,15 @@ import java.util.concurrent.TimeUnit
 object RetrofitClient {
     val client: RetrofitInterface
     val retrofit: Retrofit
-    private var access_token = ""
     private const val BASE_URL = "http://metaler.kr/"
 
-    fun setAccessToken(token: String) {
-        access_token = token
-    }
+    private val headerInterceptor = HeaderInterceptor()
 
     init {
         val okHttpClient = OkHttpClient.Builder()
             .connectTimeout(100, TimeUnit.SECONDS)
             .readTimeout(100, TimeUnit.SECONDS)
-            .addInterceptor { chain: Interceptor.Chain ->
-                val original = chain.request()
-                if (original.url.encodedPath.equals("/users/check", true)
-                    || original.url.encodedPath.equals("/users/terms", true)
-                    || original.url.encodedPath.equals("/users/join", true)
-                    || original.url.encodedPath.equals("/users/login", true)
-                ) {
-                    chain.proceed(original)
-                } else if (original.url.encodedPath.equals("/uploadFile.php", true)) {
-                    chain.proceed(
-                        original.newBuilder().url("http://file.metaler.kr/uploadFile.php").build()
-                    )
-                } else {
-                    chain.proceed(original.newBuilder().apply {
-                        addHeader("Authorization", access_token)
-                    }.build())
-                }
-            }
+            .addInterceptor(headerInterceptor)
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = if (BuildConfig.DEBUG) {
                     HttpLoggingInterceptor.Level.BODY
