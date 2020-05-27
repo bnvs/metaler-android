c56bdb98cd80e9a6e49e3bf15f6993f99e9443b4
nukeolaf <yhjin04170@gmail.com>
2020-05-28 06:25:11 +0900

    [RF] ActivityTermsAgree MVP -> MVVM 리팩토링

--- app/src/main/java/com/bnvs/metaler/view/termsagree/ActivityTermsAgree.kt
+++ app/src/main/java/com/bnvs/metaler/view/termsagree/ActivityTermsAgree.kt
@@ -2,143 +2,124 @@ package com.bnvs.metaler.view.termsagree
 
 import android.content.Intent
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.bnvs.metaler.R
-import com.bnvs.metaler.data.user.certification.model.AddUserRequest
-import com.bnvs.metaler.view.jobinput.ActivityJobInput
-import kotlinx.android.synthetic.main.activity_terms_agree.*
-import org.json.JSONObject
+import com.bnvs.metaler.data.token.source.local.TokenLocalDataSourceImpl
+import com.bnvs.metaler.data.token.source.repository.TokenRepositoryImpl
+import com.bnvs.metaler.databinding.ActivityTermsAgreeBinding
+import com.bnvs.metaler.network.NO_HEADER
+import com.bnvs.metaler.network.RetrofitClient
+import com.bnvs.metaler.network.TOKEN_EXPIRED
+import com.bnvs.metaler.view.login.ActivityLogin
+import com.bnvs.metaler.viewmodel.AddUserViewModel
 
-class ActivityTermsAgree : AppCompatActivity(),
-    ContractTermsAgree.View {
+class ActivityTermsAgree : AppCompatActivity() {
 
     private val TAG = "ActivityTermsAgree"
 
-    override lateinit var presenter: ContractTermsAgree.Presenter
+    private lateinit var binding: ActivityTermsAgreeBinding
+    private lateinit var viewModel: AddUserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms_agree)
 
-        // Create the presenter
-        presenter = PresenterTermsAgree(
-            this@ActivityTermsAgree
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_terms_agree
         )
 
-        // Set up Buttons
-        initClickListeners()
+        viewModel = ViewModelProvider(
+            this, ViewModelProvider.AndroidViewModelFactory(application)
+        ).get(AddUserViewModel::class.java)
 
-        presenter.apply {
-            start()
-            getAddUserRequest(intent)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        observeToast()
+        observeDialog()
+        observeErrorCode()
+        observeStartJobInputActivity()
+    }
+
+    private fun observeToast() {
+        viewModel.errorToastMessage.observe(
+            this,
+            Observer { message ->
+                if (message.isNotBlank()) {
+                    makeToast(message)
+                }
+            }
+        )
+    }
+
+    private fun observeDialog() {
+        viewModel.errorDialogMessage.observe(
+            this,
+            Observer { message ->
+                if (message.isNotBlank()) {
+                    makeDialog(message)
+                }
+            }
+        )
+    }
+
+    private fun observeErrorCode() {
+        viewModel.errorCode.observe(
+            this,
+            Observer { errorCode ->
+                when (errorCode) {
+                    NO_HEADER -> setAuthorizationHeader()
+                    TOKEN_EXPIRED -> startLoginActivity()
+                }
+            }
+        )
+    }
+
+    private fun observeStartJobInputActivity() {
+        viewModel.openJobInputActivity.observe(
+            this,
+            Observer {
+
+            }
+        )
+    }
+
+    private fun setAuthorizationHeader() {
+        TokenRepositoryImpl(TokenLocalDataSourceImpl(this))
+            .getAccessToken(
+                onTokenLoaded = { token ->
+                    RetrofitClient.setAccessToken(token.access_token)
+                },
+                onTokenNotExist = {
+                    startLoginActivity()
+                }
+            )
+    }
+
+    private fun startLoginActivity() {
+        finishAffinity()
+        Intent(this, ActivityLogin::class.java).also {
+            startActivity(it)
         }
     }
 
-    override fun showTermsWebView(url: String) {
-        val webView = WebView(this).apply {
-            loadUrl(url)
-            webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                    view!!.loadUrl(url)
-                    return true
-                }
-            }
-        }
+    private fun makeToast(message: String) {
+        Toast.makeText(this@ActivityTermsAgree, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun makeDialog(message: String) {
         AlertDialog.Builder(this@ActivityTermsAgree)
-            .setTitle("Metaler")
-            .setView(webView)
-            .setNegativeButton("취소") { dialog, _ ->
+            .setTitle(getString(R.string.alert))
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.allow)) { dialog, _ ->
                 dialog.dismiss()
             }
             .show()
-    }
-
-    override fun showEssentialAgreeNotCheckedDialog() {
-        AlertDialog.Builder(this).apply {
-            setTitle(getString(R.string.terms_alert))
-                .setMessage(getString(R.string.essential_terms_guide))
-                .show()
-        }
-    }
-
-    override fun makeToast(message: String) {
-        Toast.makeText(this@ActivityTermsAgree, message, Toast.LENGTH_LONG).show()
-    }
-
-    override fun showJobInputUi(addUserRequest: AddUserRequest) {
-        Intent(this, ActivityJobInput::class.java).apply {
-            putExtra("addUserRequest", addUserRequest)
-            startActivity(this)
-        }
-    }
-
-    private fun initClickListeners() {
-        setCheckBoxes()
-        setMoreButtons()
-        setNextButton()
-    }
-
-    private fun setNextButton() {
-        nextBtn.setOnClickListener { presenter.openJobInput(checkAgrees()) }
-    }
-
-    private fun setCheckBoxes() {
-        allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
-        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
-        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
-        thirdCheckBtn.setOnClickListener { onCheckChanged(thirdCheckBtn) }
-        fourthCheckBtn.setOnClickListener { onCheckChanged(fourthCheckBtn) }
-    }
-
-    private fun setMoreButtons() {
-        firstAgreeMoreBtn.setOnClickListener { presenter.openTerms(1) }
-        secondAgreeMoreBtn.setOnClickListener { presenter.openTerms(2) }
-        thirdAgreeMoreBtn.setOnClickListener { presenter.openTerms(3) }
-        fourthAgreeMoreBtn.setOnClickListener { presenter.openTerms(4) }
-    }
-
-    private fun onCheckChanged(compoundButton: CompoundButton) {
-        when (compoundButton.id) {
-            R.id.allCheckBtn -> {
-                if (allCheckBtn.isChecked) {
-                    firstCheckBtn.isChecked = true
-                    secondCheckBtn.isChecked = true
-                    thirdCheckBtn.isChecked = true
-                    fourthCheckBtn.isChecked = true
-                } else {
-                    firstCheckBtn.isChecked = false
-                    secondCheckBtn.isChecked = false
-                    thirdCheckBtn.isChecked = false
-                    fourthCheckBtn.isChecked = false
-                }
-            }
-            else -> {
-                allCheckBtn.isChecked = (
-                        firstCheckBtn.isChecked
-                                && secondCheckBtn.isChecked
-                                && thirdCheckBtn.isChecked
-                                && fourthCheckBtn.isChecked)
-            }
-        }
-    }
-
-    private fun checkAgrees(): JSONObject {
-        val result = JSONObject()
-        return if (firstCheckBtn.isChecked && secondCheckBtn.isChecked) {
-            result.run {
-                put("valid", true)
-                put("firstTerm", firstCheckBtn.isChecked)
-                put("secondTerm", secondCheckBtn.isChecked)
-                put("thirdTerm", thirdCheckBtn.isChecked)
-                put("fourthTerm", fourthCheckBtn.isChecked)
-            }
-        } else {
-            result.put("valid", false)
-        }
     }
 }
---
+++ app/src/main/java/com/bnvs/metaler/view/termsagree/TermsAgreeBindingAdapter.kt
@@ -0,0 +1,28 @@
+package com.bnvs.metaler.view.termsagree
+
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.BindingAdapter
+import com.bnvs.metaler.R
+
+@BindingAdapter("showTermsAgreeWebView")
+fun showTermsAgreeWebView(view: TextView, url: String) {
+    val webView = WebView(view.context).apply {
+        loadUrl(url)
+        webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view!!.loadUrl(url)
+                return true
+            }
+        }
+    }
+    AlertDialog.Builder(view.context)
+        .setTitle(view.context.getString(R.string.app_name))
+        .setView(webView)
+        .setPositiveButton(view.context.getString(R.string.allow)) { dialog, _ ->
+            dialog.dismiss()
+        }
+        .show()
+}
