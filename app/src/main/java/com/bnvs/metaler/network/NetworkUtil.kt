33e01ab96341389e3032228afff9db9195a9f500
nukeolaf <yhjin04170@gmail.com>
2020-06-07 00:29:37 +0900

    [MD] NetWorkUtil 함수 수정 - 기존의 retrofit 객체를 주입하는 형태로 수정함

--- app/src/main/java/com/bnvs/metaler/network/NetworkUtil.kt
+++ app/src/main/java/com/bnvs/metaler/network/NetworkUtil.kt
@@ -2,12 +2,18 @@ package com.bnvs.metaler.network
 
 import com.bnvs.metaler.data.error.ErrorResponse
 import okhttp3.ResponseBody
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 import retrofit2.HttpException
+import retrofit2.Retrofit
 
-object NetworkUtil {
+object NetworkUtil : KoinComponent {
+
+    private val retrofit: Retrofit by inject()
+
     fun getErrorMessage(e: Throwable): String {
-        return if (e is HttpException) {
-            val errorBody = e.response().errorBody()
+        return (if (e is HttpException) {
+            val errorBody = e.response()?.errorBody()
             if (errorBody != null) {
                 "[Server Error] ${e.code()} - ${e.message()} ${getErrorResponse(errorBody)}"
             } else {
@@ -15,13 +21,13 @@ object NetworkUtil {
             }
         } else {
             "$e"
-        }
+        }).toString()
     }
 
     private fun getErrorResponse(errorBody: ResponseBody): ErrorResponse {
-        return RetrofitClient.retrofit.responseBodyConverter<ErrorResponse>(
+        return retrofit.responseBodyConverter<ErrorResponse>(
             ErrorResponse::class.java,
             ErrorResponse::class.java.annotations
-        ).convert(errorBody)
+        ).convert(errorBody) ?: ErrorResponse("")
     }
 }
