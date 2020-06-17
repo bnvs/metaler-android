3d39ea212bc51a453bf3803a633cd815291a6d00
nukeolaf <yhjin04170@gmail.com>
2020-06-18 03:12:02 +0900

    [ADD] ManufacturesViewModelModule.kt 추가

---
+++ app/src/main/java/com/bnvs/metaler/module/viewmodel/ManufacturesViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.bnvs.metaler.module.viewmodel
+
+import com.bnvs.metaler.view.manufactures.ViewModelManufactures
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val manufacturesViewModelModule = module {
+    viewModel { ViewModelManufactures(get(), get()) }
+}
