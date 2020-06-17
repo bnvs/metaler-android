673345f9459ed4ab33c0a4f04a0e2dfef758ee8b
nukeolaf <yhjin04170@gmail.com>
2020-06-17 23:29:46 +0900

    [ADD] PostRequestTypeConstants.kt 추가 - 재료, 가공 탭에서 사용

---
+++ app/src/main/java/com/bnvs/metaler/util/constants/PostRequestTypeConstants.kt
@@ -0,0 +1,11 @@
+package com.bnvs.metaler.util.constants
+
+const val POST_REQUEST_TYPE = 0
+
+const val POST_REQUEST_WITH_SEARCH_TYPE_CONTENT = 1
+
+const val POST_REQUEST_WITH_SEARCH_TYPE_TAG = 2
+
+const val POST_SEARCH_TYPE_CONTENT = "content"
+
+const val POST_SEARCH_TYPE_TAG = "tag"
