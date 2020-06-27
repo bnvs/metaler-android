d0bff0a041652b523dc26072a62c3abd422fcdcb
nukeolaf <yhjin04170@gmail.com>
2020-06-27 16:02:28 +0900

    [ADD] PostSecondBindingAdapter 에 workTagInputVisibility, setTagInputItems 추가

---
+++ app/src/main/java/com/bnvs/metaler/view/addeditpost/postsecond/PostSecondBindingAdapter.kt
@@ -0,0 +1,22 @@
+package com.bnvs.metaler.view.addeditpost.postsecond
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bnvs.metaler.view.addeditpost.postsecond.tags.TagInputAdapter
+
+@BindingAdapter("workTagInputVisibility")
+fun workTagInputVisibility(view: View, categoryType: String) {
+    when (categoryType) {
+        "materials" -> view.visibility = View.GONE
+        "manufacture" -> view.visibility = View.VISIBLE
+    }
+}
+
+@BindingAdapter("setTagInputItems")
+fun setTagInputItems(view: RecyclerView, items: List<String>?) {
+    (view.adapter as? TagInputAdapter)?.run {
+        items?.let { replaceAll(it) }
+        notifyDataSetChanged()
+    }
+}
